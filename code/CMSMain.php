85313b7e498cc6be0f6a90434196490102dd41f0
Andrew O'Neil <andy@andyofniall.net>
2007-07-25 01:25:47 +0000

    Renamed WidgetAreaField to WidgetAreaEditor, made it more like FieldEditor
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@39237 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/CMSMain.php
+++ code/CMSMain.php
@@ -67,7 +67,8 @@ class CMSMain extends LeftAndMain implements CurrentPageIdentifier, PermissionPr
 		Requirements::themedCSS('typography');
 		
 		// For Blog
-		Requirements::css("blog/css/WidgetAreaField.css");
+		Requirements::css("blog/css/WidgetAreaEditor.css");
+		Requirements::javascript("blog/javascript/WidgetAreaEditor.js");
 		
 		// HACK ALERT:
 		// We need a better way of including all of the CSS that *might* be used by this application.
