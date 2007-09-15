67ac1edf87fd02b6480b65139169835c2d21a2fe
Ingo Schommer <ingo@silverstripe.com>
2007-09-15 20:11:40 +0000

    mlanthaler: Bugfix: Fixed bug which caused problems in IE but was tolerated in Firefox. (merged from branches/gsoc)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@41917 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/MemberTableField_popup.js
+++ javascript/MemberTableField_popup.js
@@ -2,12 +2,12 @@ MemberTableFieldPopupForm = Class.extend("ComplexTableFieldPopupForm");
 MemberTableFieldPopupForm.prototype = {
 	initialize: function() {
 		this.ComplexTableFieldPopupForm.initialize();
-		
+
 		Behaviour.register({
 			"form#MemberTableField_Popup_DetailForm input.action": {
 				onclick: this.submitForm.bind(this)
 			},
-			
+
 			'form#MemberTableField_Popup_DetailForm input' : {
 				initialise : function() {
 					if(this.name == 'FirstName' || this.name == 'Surname' || this.name == 'Email') {
@@ -25,18 +25,17 @@ MemberTableFieldPopupForm.prototype = {
 				afterAutocomplete : function(field, selectedItem) {
 					var data = selectedItem.getElementsByTagName('span')[1].innerHTML;
 					var items = data.split(",");
-					
+
 					this.elements.FirstName.value = items[0];
 					this.elements.Surname.value = items[1];
 					this.elements.Email.value = items[2];
 					this.elements.Password.value = items[3];
-				}		
+				}
 			}
-			
+
 			//'form#MemberTableField_Popup_DetailForm input' : AjaxMemberLookup
 		});
-	},
-	
-
+	}
 }
+
 MemberTableFieldPopupForm.applyTo('form#MemberTableField_Popup_DetailForm');
