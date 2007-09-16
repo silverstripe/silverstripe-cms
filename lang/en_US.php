92549b1331da4e82798c869271097d243dc813b8
Ingo Schommer <ingo@silverstripe.com>
2007-09-16 16:38:06 +0000

    bfojcapell: regenerated English file (merged from branches/gsoc)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@42138 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- lang/en_US.php
+++ lang/en_US.php
@@ -2,16 +2,50 @@
 
 global $lang;
 
-$lang['en_US']['LeftAndMain']['PAGETYPE'] = 'Page type: ';
+$lang['en_US']['AssetAdmin']['CHOOSEFILE'] = 'Choose file ';
+$lang['en_US']['AssetAdmin']['UPLOAD'] = 'Upload Files Listed Below';
+$lang['en_US']['AssetAdmin']['FILESREADY'] = 'Files ready to upload:';
+$lang['en_US']['AssetAdmin']['ONLYADMINS'] = 'Only administrators can upload %s files.';
+$lang['en_US']['AssetAdmin']['TOOLARGE'] = '%s is too large (%s). Files of this type cannot be larger than %s';
+$lang['en_US']['AssetAdmin']['UPLOADEDX'] = 'Uploaded %s files';
+$lang['en_US']['AssetAdmin']['NOTHINGTOUPLOAD'] = 'There was nothing to upload';
+$lang['en_US']['AssetAdmin']['DELSELECTED'] = 'Delete selected files';
+$lang['en_US']['AssetAdmin']['TYPE'] = 'Type';
+$lang['en_US']['AssetAdmin']['CREATED'] = 'First Uploaded';
+$lang['en_US']['AssetAdmin']['LASTEDITED'] = 'Last Updated';
+$lang['en_US']['AssetAdmin']['SAVEFOLDERNAME'] = 'Save folder name';
+$lang['en_US']['AssetAdmin']['OWNER'] = 'Owner';
+$lang['en_US']['AssetAdmin']['CONTENTUSABLEBY'] = 'Content usable by';
+$lang['en_US']['AssetAdmin']['CONTENTMODBY'] = 'Content modifiable by';
+$lang['en_US']['AssetAdmin']['MOVEDX'] = 'Moved %s files';
+$lang['en_US']['AssetAdmin']['NOWBROKEN'] = 'These pages now have broken links:';
+$lang['en_US']['AssetAdmin']['DELETEDX'] = 'Deleted %s files.%s';
+$lang['en_US']['AssetAdmin']['SAVEDFILE'] = 'Saved file %s';
+$lang['en_US']['AssetAdmin']['NEWFOLDER'] = 'NewFolder';
+$lang['en_US']['AssetTableField']['TITLE'] = 'Title';
+$lang['en_US']['AssetTableField']['FILENAME'] = 'Filename';
+$lang['en_US']['AssetTableField']['TYPE'] = 'Type';
+$lang['en_US']['AssetTableField']['SIZE'] = 'Size';
+$lang['en_US']['AssetTableField']['OWNER'] = 'Owner';
+$lang['en_US']['AssetTableField']['CREATED'] = 'First uploaded';
+$lang['en_US']['AssetTableField']['LASTEDIT'] = 'Last changed';
+$lang['en_US']['AssetTableField']['DIM'] = 'Dimensions';
+$lang['en_US']['AssetTableField']['PAGESLINKING'] = 'The following pages link to this file:';
+$lang['en_US']['AssetTableField']['NOLINKS'] = 'This file hasn\'t been linked to from any pages.';
+$lang['en_US']['BulkLoaderAdmin']['CSVFILE'] = 'CSV File';
+$lang['en_US']['BulkLoaderAdmin']['PREVIEW'] = 'Preview';
+$lang['en_US']['BulkLoaderAdmin']['PRESSCNT'] = 'Press continue to load this data in';
+$lang['en_US']['BulkLoaderAdmin']['CONFIRMBULK'] = 'Confirm bulk load';
+$lang['en_US']['BulkLoaderAdmin']['DATALOADED'] = 'This data has been loaded in';
 $lang['en_US']['LeftAndMain']['SITECONTENT'] = array(
 	'Site Content',
 	PR_HIGH,
 	'Root node on left'
 );
 $lang['en_US']['CMSMain']['CREATE'] = array(
-	'Create a ',
+	'Create ',
 	PR_MEDIUM,
-	'"Create a " message, followed by an action (e.g. "contact form")'
+	'"Create " message, followed by an action (e.g. "a contact form")'
 );
 $lang['en_US']['CMSMain']['REMOVEDFD'] = 'Removed from the draft site';
 $lang['en_US']['CMSMain']['RESTORE'] = 'Restore';
@@ -53,10 +87,10 @@ $lang['en_US']['CMSMain']['COMPARINGV'] = 'You are comparing versions #%d and #%
 $lang['en_US']['CMSMain']['COPYPUBTOSTAGE'] = 'Do you really want to copy the published content to the stage site?';
 $lang['en_US']['CMSMain']['OK'] = 'OK';
 $lang['en_US']['CMSMain']['CANCEL'] = 'Cancel';
-$lang['en_US']['CMSMain']['PAGEPUB'] = '%d page published ';
 $lang['en_US']['CMSMain']['PAGESPUB'] = '%d pages published ';
-$lang['en_US']['CMSMain']['PAGEDEL'] = '%d page deleted ';
+$lang['en_US']['CMSMain']['PAGEPUB'] = '%d page published ';
 $lang['en_US']['CMSMain']['PAGESDEL'] = '%d pages deleted ';
+$lang['en_US']['CMSMain']['PAGEDEL'] = '%d page deleted ';
 $lang['en_US']['CMSMain']['NOWBROKEN'] = '  The following pages now have broken links:';
 $lang['en_US']['CMSMain']['NOWBROKEN2'] = 'Their owners have been emailed and they will fix up those pages.';
 $lang['en_US']['CMSMain']['GO'] = 'Go';
@@ -105,6 +139,11 @@ $lang['en_US']['LeftAndMain']['SECURITY'] = array(
 	PR_HIGH,
 	'Menu title'
 );
+$lang['en_US']['LeftAndMain']['COMMENTS'] = array(
+	'Comments',
+	 PR_HIGH,
+	'Menu title'
+);
 $lang['en_US']['LeftAndMain']['STATISTICS'] = array(
 	'Statistics',
 	PR_HIGH,
@@ -116,17 +155,93 @@ $lang['en_US']['LeftAndMain']['HELP'] = array(
 	'Menu title'
 );
 $lang['en_US']['LeftAndMain']['PAGETYPE'] = 'Page type: ';
-$lang['en_US']['LeftAndMain']['SITECONTENT'] = array(
-	'Site Content',
-	PR_HIGH,
-	'Root node on left'
-);
 $lang['en_US']['LeftAndMain']['SAVEDUP'] = 'Saved';
 $lang['en_US']['LeftAndMain']['CHANGEDURL'] = '  Changed URL to \'%s\'';
 $lang['en_US']['LeftAndMain']['STATUSTO'] = '  Status changed to \'%s\'';
 $lang['en_US']['LeftAndMain']['SAVED'] = 'saved';
 $lang['en_US']['LeftAndMain']['PLEASESAVE'] = 'Please Save Page: This page could not be upated because it hasn\'t been saved yet.';
 $lang['en_US']['LeftAndMain']['REQUESTERROR'] = 'Error in request';
+$lang['en_US']['MemberList']['FN'] = 'First Name';
+$lang['en_US']['MemberList']['SN'] = 'Surname';
+$lang['en_US']['MemberList']['EMAIL'] = 'Email';
+$lang['en_US']['MemberList']['ADD'] = 'Add';
+$lang['en_US']['MemberList']['PASSWD'] = 'Password';
+$lang['en_US']['MemberList']['SEARCH'] = 'Search';
+$lang['en_US']['MemberList']['FILTERBYG'] = 'Filter by group';
+$lang['en_US']['MemberTableField']['ADDEDTOGROUP'] = 'Added member to group';
+$lang['en_US']['MemberTableField']['ADD'] = 'Add';
+$lang['en_US']['NewsletterAdmin']['NEWSLTYPE'] = 'Newsletter Type';
+$lang['en_US']['NewsletterAdmin']['FROMEM'] = 'From email address';
+$lang['en_US']['NewsletterAdmin']['RESEND'] = 'Resend';
+$lang['en_US']['NewsletterAdmin']['SEND'] = 'Send...';
+$lang['en_US']['NewsletterAdmin']['SAVE'] = 'Save';
+$lang['en_US']['NewsletterAdmin']['SENDING'] = 'Sending emails...';
+$lang['en_US']['NewsletterAdmin']['SENTTESTTO'] = 'Sent test to ';
+$lang['en_US']['NewsletterAdmin']['PLEASEENTERMAIL'] = 'Please enter an email address';
+$lang['en_US']['NewsletterAdmin']['SAVED'] = 'Saved';
+$lang['en_US']['NewsletterAdmin']['NEWLIST'] = 'New mailing list';
+$lang['en_US']['NewsletterAdmin']['NEWNEWSLTYPE'] = 'New newsletter type';
+$lang['en_US']['NewsletterAdmin']['MEWDRAFTMEWSL'] = 'New draft newsletter';
+$lang['en_US']['NewsletterAdmin']['SHOWCONTENTS'] = 'Show contents';
+$lang['en_US']['SecurityAdmin']['ADVANCEDONLY'] = 'This section is for advanced users only.
+							See <a href=\"http://doc.silverstripe.com/doku.php?id=permissions:codes\" target=\"_blank\">this page</a>
+							for more information.';
+$lang['en_US']['SecurityAdmin']['ADDMEMBER'] = 'Add Member';
+$lang['en_US']['SecurityAdmin']['SAVE'] = 'Save';
+$lang['en_US']['SecurityAdmin']['SGROUPS'] = 'Security groups';
+$lang['en_US']['SecurityAdmin']['NEWGROUP'] = 'New Group';
+$lang['en_US']['SideReport']['REPEMPTY'] = array(
+	'The %s report is empty.',
+	PR_MEDIUM,
+	'%s is a report title'
+);
+$lang['en_US']['SideReport']['EMPTYPAGES'] = 'Empty pages';
+$lang['en_US']['SideReport']['LAST2WEEKS'] = 'Pages edited in the last 2 weeks';
+$lang['en_US']['StaticExporter']['NAME'] = 'Static exporter';
+$lang['en_US']['StaticExporter']['FOLDEREXPORT'] = 'Folder to export to';
+$lang['en_US']['StaticExporter']['BASEURL'] = 'Base URL';
+$lang['en_US']['StaticExporter']['EXPORTTOTHAT'] = 'Export to that folder';
+$lang['en_US']['StaticExporter']['ONETHATEXISTS'] = 'Please specify a folder that exists';
+$lang['en_US']['ThumbnailStripField']['NOTAFOLDER'] = 'This is not a folder';
+$lang['en_US']['AssetAdmin_uploadiframe.ss']['PERMFAILED'] = 'You do not have permission to upload files into this folder.';
+$lang['en_US']['BulkLoaderAdmin_preview.ss']['RES'] = 'Results';
+$lang['en_US']['CMSMain_versions.ss']['WHEN'] = 'When';
+$lang['en_US']['CMSMain_versions.ss']['AUTHOR'] = 'Author';
+$lang['en_US']['CMSMain_versions.ss']['PUBR'] = 'Publisher';
+$lang['en_US']['CMSMain_versions.ss']['UNKNOWN'] = 'Unknown';
+$lang['en_US']['CMSMain_versions.ss']['NOTPUB'] = 'Not published';
+$lang['en_US']['CommentList.ss']['NOCOM'] = 'There are no comments on this page.';
+$lang['en_US']['CommentList.ss']['CREATEDW'] = 'Comments are created whenever one of the \'workflow actions\'
+	are undertaken - Publish, Reject, Submit.';
+$lang['en_US']['ImageEditor.ss']['UNTITLED'] = 'Untitled Document';
+$lang['en_US']['ImageEditor.ss']['SAVE'] = 'save&nbsp;image';
+$lang['en_US']['ImageEditor.ss']['UNDO'] = 'undo';
+$lang['en_US']['ImageEditor.ss']['REDO'] = 'redo';
+$lang['en_US']['ImageEditor.ss']['ROT'] = 'rotate';
+$lang['en_US']['ImageEditor.ss']['CROP'] = 'crop';
+$lang['en_US']['ImageEditor.ss']['WIDTH'] = 'width';
+$lang['en_US']['ImageEditor.ss']['HEIGHT'] = 'height';
+$lang['en_US']['ImageEditor.ss']['CANCEL'] = 'cancel';
+$lang['en_US']['AssetAdmin_left.ss']['FOLDERS'] = 'Folders';
+$lang['en_US']['AssetAdmin_left.ss']['CREATE'] = 'Create';
+$lang['en_US']['AssetAdmin_left.ss']['DELETE'] = 'Delete...';
+$lang['en_US']['AssetAdmin_left.ss']['REORDER'] = 'Reorder...';
+$lang['en_US']['AssetAdmin_left.ss']['GO'] = 'Go';
+$lang['en_US']['AssetAdmin_left.ss']['SELECTTODEL'] = 'Select the folders that you want to delete and then click the button below';
+$lang['en_US']['AssetAdmin_left.ss']['DELFOLDERS'] = 'Delete the selected folders';
+$lang['en_US']['AssetAdmin_left.ss']['TOREORG'] = 'To reorganise your folders, drag them around as desired.';
+$lang['en_US']['AssetAdmin_right.ss']['WELCOME'] = 'Welcome to';
+$lang['en_US']['AssetAdmin_right.ss']['CHOOSEPAGE'] = 'Please choose a page from the left.';
+$lang['en_US']['AssetTableField.ss']['DRAGTOFOLDER'] = 'Drag to folder on left to move file';
+$lang['en_US']['AssetTableField.ss']['DELFILE'] = 'Delete this file';
+$lang['en_US']['BulkLoaderAdmin_left.ss']['FUNCTIONS'] = 'Functions';
+$lang['en_US']['BulkLoaderAdmin_left.ss']['BATCHEF'] = 'Batch entry functions';
+$lang['en_US']['CMSLeft.ss']['SITECONT'] = 'Site Content';
+$lang['en_US']['CMSLeft.ss']['NEWPAGE'] = 'New Page...';
+$lang['en_US']['CMSLeft.ss']['DELPAGE'] = 'Delete Pages...';
+$lang['en_US']['CMSLeft.ss']['GO'] = 'Go';
+$lang['en_US']['CMSLeft.ss']['SELECTPAGESDEL'] = 'Select the pages that you want to delete and then click the button below';
+$lang['en_US']['CMSLeft.ss']['DELPAGES'] = 'Delete the selected pages';
 $lang['en_US']['CMSMain_left.ss']['OPENBOX'] = 'click to open this box';
 $lang['en_US']['CMSMain_left.ss']['CLOSEBOX'] = 'click to close box';
 $lang['en_US']['CMSMain_left.ss']['SITECONTENT TITLE'] = array(
@@ -149,7 +264,6 @@ $lang['en_US']['CMSMain_left.ss']['ENABLEDRAGGING'] = array(
 	'Allow drag &amp; drop reordering',
 	 PR_HIGH
 );
-$lang['en_US']['CMSMain_left.ss']['SEARCH'] = 'Search';
 $lang['en_US']['CMSMain_left.ss']['SEARCHTITLE'] = 'Search through URL, Title, Menu Title, &amp; Content';
 $lang['en_US']['CMSMain_left.ss']['EDITEDSINCE'] = 'Edited Since';
 $lang['en_US']['CMSMain_left.ss']['ADDSEARCHCRITERIA'] = 'Add Criteria...';
@@ -181,8 +295,87 @@ $lang['en_US']['CMSMain_right.ss']['MESSAGE'] = 'Message';
 $lang['en_US']['CMSMain_right.ss']['SUBMIT'] = 'Submit for approval';
 $lang['en_US']['CMSMain_right.ss']['WELCOMETO'] = 'Welcome to';
 $lang['en_US']['CMSMain_right.ss']['CHOOSEPAGE'] = 'Please choose a page from the left.';
+$lang['en_US']['CMSRight.ss']['ECONTENT'] = 'Edit Content';
 $lang['en_US']['CMSRight.ss']['WELCOMETO'] = 'Welcome to';
 $lang['en_US']['CMSRight.ss']['CHOOSEPAGE'] = 'Please choose a page from the left.';
+$lang['en_US']['GenericDataAdmin_left.ss']['ADDLISTING'] = 'Add Listing';
+$lang['en_US']['GenericDataAdmin_left.ss']['SEARCHLISTINGS'] = 'Search Listings';
+$lang['en_US']['GenericDataAdmin_left.ss']['SEARCHRESULTS'] = 'Search Results';
+$lang['en_US']['MemberList_Table.ss']['FN'] = 'First Name';
+$lang['en_US']['MemberList_Table.ss']['SN'] = 'Surname';
+$lang['en_US']['MemberList_Table.ss']['EMAIL'] = 'Email Address';
+$lang['en_US']['MemberList_Table.ss']['PASSWD'] = 'Password';
+$lang['en_US']['MemberTableField.ss']['ADDNEW'] = array(
+	'Add new',
+	50,
+	'Followed by a member type'
+);
+$lang['en_US']['MemberTableField.ss']['SHOWMEMBER'] = 'Show this member';
+$lang['en_US']['MemberTableField.ss']['EDITMEMBER'] = 'Edit this member';
+$lang['en_US']['MemberTableField.ss']['DELETEMEMBER'] = 'Delete this member';
+$lang['en_US']['NewsletterAdmin_BouncedList.ss']['HAVEBOUNCED'] = 'Emails that have bounced';
+$lang['en_US']['NewsletterAdmin_BouncedList.ss']['UNAME'] = 'User name';
+$lang['en_US']['NewsletterAdmin_BouncedList.ss']['EMADD'] = 'Email address';
+$lang['en_US']['NewsletterAdmin_BouncedList.ss']['NOBOUNCED'] = 'No emails sent have bounced.';
+$lang['en_US']['NewsletterAdmin_SiteTree.ss']['DRAFTS'] = 'Drafts';
+$lang['en_US']['NewsletterAdmin_SiteTree.ss']['SENT'] = 'Sent Items';
+$lang['en_US']['NewsletterAdmin_SiteTree.ss']['MAILLIST'] = 'Mailing List';
+$lang['en_US']['NewsletterAdmin_UnsubscribedList.ss']['UNAME'] = 'User name';
+$lang['en_US']['NewsletterAdmin_UnsubscribedList.ss']['UNSUBON'] = 'Unsubscribed on';
+$lang['en_US']['NewsletterAdmin_UnsubscribedList.ss']['NOUNSUB'] = 'No users have unsubscribed from this newsletter.';
+$lang['en_US']['NewsletterAdmin_left.ss']['NEWSLETTERS'] = 'Newsletters';
+$lang['en_US']['NewsletterAdmin_left.ss']['CREATE'] = 'Create...';
+$lang['en_US']['NewsletterAdmin_left.ss']['DEL'] = 'Delete...';
+$lang['en_US']['NewsletterAdmin_left.ss']['ADDTYPE'] = 'Add new type';
+$lang['en_US']['NewsletterAdmin_left.ss']['ADDDRAFT'] = 'Add new draft';
+$lang['en_US']['NewsletterAdmin_left.ss']['GO'] = 'Go';
+$lang['en_US']['NewsletterAdmin_left.ss']['SELECTDRAFTS'] = 'Select the drafts that you want to delete and then click the button below';
+$lang['en_US']['NewsletterAdmin_left.ss']['DELETEDRAFTS'] = 'Delete the selected drafts';
+$lang['en_US']['NewsletterAdmin_right.ss']['SENDTEST'] = 'Send test to';
+$lang['en_US']['NewsletterAdmin_right.ss']['ENTIRE'] = 'Send to the entire mailing list';
+$lang['en_US']['NewsletterAdmin_right.ss']['ONLYNOT'] = 'Send to only people not previously sent to';
+$lang['en_US']['NewsletterAdmin_right.ss']['CANCEL'] = 'Cancel';
+$lang['en_US']['NewsletterAdmin_right.ss']['SEND'] = 'Send newsletter';
+$lang['en_US']['NewsletterAdmin_right.ss']['WELCOME1'] = array(
+	'Welcome to the',
+	50,
+	'Followed by application name'
+);
+$lang['en_US']['NewsletterAdmin_right.ss']['WELCOME2'] = array(
+	'newsletter admininistration section.  Please choose a folder from the left.',
+	50
+);
+$lang['en_US']['ReportAdmin_SiteTree.ss']['REPORTS'] = 'Reports';
+$lang['en_US']['ReportAdmin_left.ss']['REPORTS'] = 'Reports';
+$lang['en_US']['ReportAdmin_right.ss']['WELCOME1'] = array(
+	'Welcome to the',
+	50,
+	'Followed by application name'
+);
+$lang['en_US']['ReportAdmin_right.ss']['WELCOME2'] = array(
+	'reporting section.  Please choose a specific report from the left.',
+	50
+);
+$lang['en_US']['SecurityAdmin_left.ss']['SECGROUPS'] = 'Security Groups';
+$lang['en_US']['SecurityAdmin_left.ss']['CREATE'] = 'Create';
+$lang['en_US']['SecurityAdmin_left.ss']['DEL'] = 'Delete...';
+$lang['en_US']['SecurityAdmin_left.ss']['REORDER'] = 'Reorder...';
+$lang['en_US']['SecurityAdmin_left.ss']['GO'] = 'Go';
+$lang['en_US']['SecurityAdmin_left.ss']['SELECT'] = 'Select the pages that you want to delete and then click the button below';
+$lang['en_US']['SecurityAdmin_left.ss']['DELGROUPS'] = 'Delete the selected groups';
+$lang['en_US']['SecurityAdmin_left.ss']['TOREORG'] = 'To reorganise your site, drag the pages around as desired.';
+$lang['en_US']['SecurityAdmin_right.ss']['WELCOME1'] = array(
+	'Welcome to the',
+	50,
+	'Followed by application name'
+);
+$lang['en_US']['SecurityAdmin_right.ss']['WELCOME2'] = array(
+	'security admininistration section.  Please choose a group from the left.',
+	50
+);
+$lang['en_US']['StatisticsAdmin_left.ss']['REPTYPES'] = 'Report Types';
+$lang['en_US']['StatisticsAdmin_left.ss']['OVERV'] = 'Overview';
+$lang['en_US']['StatisticsAdmin_left.ss']['USERS'] = 'Users';
 $lang['en_US']['LeftAndMain.ss']['LOADING'] = array(
 	'Loading...',
 	PR_HIGH
@@ -198,5 +391,52 @@ $lang['en_US']['LeftAndMain.ss']['EDIT'] = 'Edit';
 $lang['en_US']['LeftAndMain.ss']['DRAFTS'] = 'Draft Site';
 $lang['en_US']['LeftAndMain.ss']['PUBLIS'] = 'Published Site';
 $lang['en_US']['LeftAndMain.ss']['ARCHS'] = 'Archived Site';
+$lang['en_US']['MemberList.ss']['FILTER'] = array(
+	'Filter',
+	50,
+	'Filter as a verb'
+);
+$lang['en_US']['NewsletterList.ss']['CHOOSEDRAFT1'] = 'Please choose a draft on the left, or';
+$lang['en_US']['NewsletterList.ss']['CHOOSEDRAFT2'] = 'add one';
+$lang['en_US']['NewsletterList.ss']['CHOOSESENT'] = 'Please choose a sent item on the left.';
+$lang['en_US']['Newsletter_RecipientImportField.ss']['IMPORTNEW'] = 'Imported new members';
+$lang['en_US']['Newsletter_RecipientImportField.ss']['IMPORTED'] = 'New members imported:';
+$lang['en_US']['Newsletter_RecipientImportField.ss']['UPDATED'] = 'Members updated:';
+$lang['en_US']['Newsletter_RecipientImportField.ss']['CHANGED'] = 'Number of details changed:';
+$lang['en_US']['Newsletter_RecipientImportField.ss']['SKIPPED'] = 'Records skipped:';
+$lang['en_US']['Newsletter_RecipientImportField.ss']['TIME'] = 'Time taken:';
+$lang['en_US']['Newsletter_RecipientImportField.ss']['SEC'] = 'seconds';
+$lang['en_US']['Newsletter_RecipientImportField_Table.ss']['CONTENTSOF'] = 'Contents of';
+$lang['en_US']['Newsletter_RecipientImportField_Table.ss']['YES'] = 'Confirm';
+$lang['en_US']['Newsletter_RecipientImportField_Table.ss']['NO'] = 'Cancel';
+$lang['en_US']['Newsletter_RecipientImportField_Table.ss']['RECIMPORTED'] = 'Recipients imported from';
+$lang['en_US']['Newsletter_SentStatusReport.ss']['SENDFAIL'] = 'Sending to the Following Recipients Failed';
+$lang['en_US']['Newsletter_SentStatusReport.ss']['EMAIL'] = 'Email';
+$lang['en_US']['Newsletter_SentStatusReport.ss']['DATE'] = 'Date';
+$lang['en_US']['Newsletter_SentStatusReport.ss']['RES'] = 'Result';
+$lang['en_US']['Newsletter_SentStatusReport.ss']['SENDBOUNCED'] = 'Sending to the Following Recipients Bounced';
+$lang['en_US']['Newsletter_SentStatusReport.ss']['NEWSNEVERSENT'] = 'The Newsletter has Never Been Sent to Following Subscribers';
+$lang['en_US']['Newsletter_SentStatusReport.ss']['FN'] = 'Firstname';
+$lang['en_US']['Newsletter_SentStatusReport.ss']['SN'] = 'Surname';
+$lang['en_US']['Newsletter_SentStatusReport.ss']['SENTOK'] = 'Sending to the Following Recipients was Successful';
+$lang['en_US']['PageCommentInterface.ss']['POSTCOM'] = 'Post your comment';
+$lang['en_US']['PageCommentInterface.ss']['COMMENTS'] = 'Comments';
+$lang['en_US']['PageCommentInterface.ss']['PREV'] = 'previous';
+$lang['en_US']['PageCommentInterface.ss']['NEXT'] = 'next';
+$lang['en_US']['PageCommentInterface.ss']['NOCOMMENTSYET'] = 'No one has commented on this page yet.';
+$lang['en_US']['PageCommentInterface_singlecomment.ss']['PBY'] = 'Posted by';
+$lang['en_US']['PageCommentInterface_singlecomment.ss']['ISSPAM'] = 'this comment is spam';
+$lang['en_US']['PageCommentInterface_singlecomment.ss']['ISNTSPAM'] = 'this comment is not spam';
+$lang['en_US']['PageCommentInterface_singlecomment.ss']['REMCOM'] = 'remove this comment';
+$lang['en_US']['TaskList.ss']['BY'] = 'by';
+$lang['en_US']['ThumbnailStripField.ss']['CHOOSEFOLDER'] = '(Choose a folder above)';
+$lang['en_US']['WaitingOn.ss']['ATO'] = 'assigned to';
+$lang['en_US']['SubmittedFormEmail.ss']['SUBMITTED'] = 'The following data was submitted to your website:';
+$lang['en_US']['ViewArchivedEmail.ss']['HAVEASKED'] = array(
+	'You have asked to view the content of our site on',
+	30,
+	'Followed by a date'
+);
+$lang['en_US']['ViewArchivedEmail.ss']['CANACCESS'] = 'You can access the archived site at this link:';
 
 ?>
