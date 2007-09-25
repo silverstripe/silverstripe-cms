bc246bf484af58c886412fea7a214b21e0b2a845
Sam Minnee <sam@silverstripe.com>
2007-09-25 03:44:07 +0000

    Updated and improved StaticExporter
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@42536 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/StaticExporter.php
+++ code/StaticExporter.php
@@ -8,81 +8,98 @@
 class StaticExporter extends Controller {
 	function init() {
 		parent::init();
-		if(!$this->can('AdminCMS')) {
+		if(!Permission::check('ADMIN')) {
 			$messageSet = array(
-				'default' => _t('LeftAndMain.PERMDEFAULT'),
-				'alreadyLoggedIn' => _t('LeftAndMain.PERMALREADY'),
-				'logInAgain' => _t('LeftAndMain.PERMAGAIN'),
+				'default' => "Enter your email address and password to access the CMS.",
+				'alreadyLoggedIn' => "I'm sorry, but you can't access that part of the CMS.  If you want to log in as someone else, do so below",
+				'logInAgain' => "You have been logged out of the CMS.  If you would like to log in again, enter a username and password below.",
 			);
 
 			Security::permissionFailure($this, $messageSet);
+			return;
 		}
 	}
-
-
+		
+	
 	function Link($action = null) {
 		return "StaticExporter/$action";
 	}
-
+	
 	function index() {
-		echo "<h1>"._t('StaticExporter.NAME','Static exporter')."</h1>";
+		echo "<h1>Static exporter</h1>";
 		echo $this->StaticExportForm()->forTemplate();
 	}
-
+	
 	function StaticExportForm() {
 		return new Form($this, 'StaticExportForm', new FieldSet(
-			new TextField('folder', _t('StaticExporter.FOLDEREXPORT','Folder to export to')),
-			new TextField('baseurl', _t('StaticExporter.BASEURL','Base URL'))
+			// new TextField('folder', 'Folder to export to'),
+			new TextField('baseurl', 'Base URL')
 		), new FieldSet(
-			new FormAction('export', _t('StaticExporter.EXPORTTOTHAT','Export to that folder'))
+			new FormAction('export', 'Export to that folder')
 		));
 	}
-
+	
 	function export() {
-
+		
 		if($_REQUEST['baseurl']) {
 			$base = $_REQUEST['baseurl'];
 			if(substr($base,-1) != '/') $base .= '/';
 			Director::setBaseURL($base);
 		}
+		
+		$folder = '/tmp/static-export/' . project();
+		if(!project()) $folder .= 'site';
+		if(!file_exists($folder)) mkdir($folder, 02775, true);
 
-		$folder = $_REQUEST['folder'];
+		$f1 = Director::baseFolder() . '/assets';
+		$f2 = Director::baseFolder() . '/' . project();
+		`cd $folder; ln -s $f1; ln -s $f2`;
+
+		
+		$baseFolder = basename($folder);
+		
 		if($folder && file_exists($folder)) {
 			$pages = DataObject::get("SiteTree");
 			foreach($pages as $page) {
 				$subfolder = "$folder/$page->URLSegment";
 				$contentfile = "$folder/$page->URLSegment/index.html";
-
-				echo "<li>$page->URLSegment/index.html";
-
-				// Make the folder
+				
+				// Make the folder				
 				if(!file_exists($subfolder)) {
-					mkdir($subfolder);
-					chmod($subfolder,02775);
+					mkdir($subfolder, 02775);
 				}
-
+				
 				// Run the page
 				Requirements::clear();
 				$controllerClass = "{$page->class}_Controller";
 				if(class_exists($controllerClass)) {
 					$controller = new $controllerClass($page);
-
-					$response = $controller->run( array() );
-					$pageContent = $response->getBody();
+					$pageContent = $controller->run( array() );
 					
 					// Write to file
 					if($fh = fopen($contentfile, 'w')) {
-						fwrite($fh, $pageContent);
+						fwrite($fh, $pageContent->getBody());
 						fclose($fh);
 					}
 				}
 			}
+			
+			copy("$folder/home/index.html", "$folder/index.html");			
+
+			`cd /tmp/static-export; tar -czhf $baseFolder.tar.gz $baseFolder`;
+
+			$content = file_get_contents("/tmp/static-export/$baseFolder.tar.gz");
+			Filesystem::removeFolder('/tmp/static-export');
+
+			HTTP::sendFileToBrowser($content, "$baseFolder.tar.gz");
+			return null;
+			
 		} else {
-			echo _t('StaticExporter.ONETHATEXISTS',"Please specify a folder that exists");
+			echo "Please specify a folder that exists";
 		}
-
+			
 	}
-
+	
 }
 
 ?>
