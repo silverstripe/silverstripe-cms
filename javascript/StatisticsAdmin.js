5e41feeca177765218e2608a403cbc23f221e333
Andrew O'Neil <andy@andyofniall.net>
2007-10-25 01:03:17 +0000

    GSOC #116 - Refactor StatisticsAdmin
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@43831 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/StatisticsAdmin.js
+++ javascript/StatisticsAdmin.js
@@ -1,127 +1,129 @@
-defaultOpts = {
-	fillOpacity:			1.0,
-	axis: {
-				lineWidth:			1.0,
-				lineColor:			'#000000',
-				tickSize:			3.0,
-				labelColor:			'#666666',
-				labelFont:			'Tahoma',
-				labelFontSize:		20,
-				labelWidth: 		50.0
-		},
-
-	padding: {left: 30, right: 0, top: 10, bottom: 30},
-
-	backgroundColor:		'#cccccc',
-	colorScheme:			'blue'
-}
-
-showCT = function() {
-	console.log('asdg');
-	if($('browserchart')) {
-		var bchart = new Plotr.PieChart('bchart', defaultOpts);
-		bchart.addDataset(bchartdata);
-		bchart.render();
-		console.log('bchart rendered');
+StatisticsAdmin = Class.create();
+StatisticsAdmin = {
+	defaultOpts:  {
+		fillOpacity:			1.0,
+		axis: {
+					lineWidth:			1.0,
+					lineColor:			'#000000',
+					tickSize:			3.0,
+					labelColor:			'#666666',
+					labelFont:			'Tahoma',
+					labelFontSize:		20,
+					labelWidth: 		50.0
+			},
+	
+		padding: {left: 30, right: 0, top: 10, bottom: 30},
+	
+		backgroundColor:		'#cccccc',
+		colorScheme:			'blue'
+	},
+	
+	showCT : function() {
+		console.log('asdg');
+		if($('browserchart')) {
+			var bchart = new Plotr.PieChart('bchart', defaultOpts);
+			bchart.addDataset(bchartdata);
+			bchart.render();
+			console.log('bchart rendered');
+		}
+		if($('trendchart')) {
+			var tchart = new Plotr.LineChart('tchart', defaultOpts);
+			tchart.addDataset(tchartdata);
+			tchart.render();
+			console.log('tchart rendered');
+		}
+		if($('usertable') || $('viewtable')) {
+			fdTableSort.init();
+			tablePaginater.init();
+			console.log('table rendered');
+		}
+	},
+	
+	overview : function() {
+		$('browserchart') ? $('browserchart').hide() : null;
+		$('trendchart') ? $('trendchart').hide() : null;
+		$('usertable') ? $('usertable').hide() : null;
+		$('viewtable') ? $('viewtable').hide() : null;
+		$('oschart') ? $('oschart').hide() : null;
+		$('uacchart') ? $('uacchart').hide() : null;
+		$('bovs') ? $('bovs').show() : null;
+	},
+	
+	users : function() {
+		$('browserchart') ? $('browserchart').hide() : null;
+		$('trendchart') ? $('trendchart').hide() : null;
+		$('usertable') ? $('usertable').show() : null;
+		$('viewtable') ? $('viewtable').hide() : null;
+		$('oschart') ? $('oschart').hide() : null;
+		$('uacchart') ? $('uacchart').show() : null;
+		$('bovs') ? $('bovs').hide() : null;
+	},
+	
+	views : function() {
+		$('browserchart') ? $('browserchart').hide() : null;
+		$('trendchart') ? $('trendchart').hide() : null;
+		$('usertable') ? $('usertable').hide() : null;
+		$('viewtable') ? $('viewtable').show() : null;
+		$('oschart') ? $('oschart').hide() : null;
+		$('uacchart') ? $('uacchart').hide() : null;
+		$('bovs') ? $('bovs').hide() : null;
+	},
+	
+	trends : function() {
+		$('browserchart') ? $('browserchart').hide() : null;
+		$('trendchart') ? $('trendchart').show() : null;
+		$('usertable') ? $('usertable').hide() : null;
+		$('viewtable') ? $('viewtable').hide() : null;
+		$('oschart') ? $('oschart').hide() : null;
+		$('uacchart') ? $('uacchart').hide() : null;
+		$('bovs') ? $('bovs').hide() : null;
+	},
+	
+	browsers : function() {
+		$('browserchart') ? $('browserchart').show() : null;
+		$('trendchart') ? $('trendchart').hide() : null;
+		$('usertable') ? $('usertable').hide() : null;
+		$('viewtable') ? $('viewtable').hide() : null;
+		$('oschart') ? $('oschart').hide() : null;
+		$('uacchart') ? $('uacchart').hide() : null;
+		$('bovs') ? $('bovs').hide() : null;
+	},
+	
+	os : function() {
+		$('browserchart') ? $('browserchart').hide() : null;
+		$('trendchart') ? $('trendchart').hide() : null;
+		$('usertable') ? $('usertable').hide() : null;
+		$('viewtable') ? $('viewtable').hide() : null;
+		$('oschart') ? $('oschart').show() : null;
+		$('uacchart') ? $('uacchart').hide() : null;
+		$('bovs') ? $('bovs').hide() : null;
 	}
-	if($('trendchart')) {
-		var tchart = new Plotr.LineChart('tchart', defaultOpts);
-		tchart.addDataset(tchartdata);
-		tchart.render();
-		console.log('tchart rendered');
-	}
-	if($('usertable') || $('viewtable')) {
-		fdTableSort.init();
-		tablePaginater.init();
-		console.log('table rendered');
-	}
-}
-
-
-overview = function() {
-	$('browserchart') ? $('browserchart').hide() : null;
-	$('trendchart') ? $('trendchart').hide() : null;
-	$('usertable') ? $('usertable').hide() : null;
-	$('viewtable') ? $('viewtable').hide() : null;
-	$('oschart') ? $('oschart').hide() : null;
-	$('uacchart') ? $('uacchart').hide() : null;
-	$('bovs') ? $('bovs').show() : null;
-}
-
-users = function() {
-	$('browserchart') ? $('browserchart').hide() : null;
-	$('trendchart') ? $('trendchart').hide() : null;
-	$('usertable') ? $('usertable').show() : null;
-	$('viewtable') ? $('viewtable').hide() : null;
-	$('oschart') ? $('oschart').hide() : null;
-	$('uacchart') ? $('uacchart').show() : null;
-	$('bovs') ? $('bovs').hide() : null;
-}
-
-views = function() {
-	$('browserchart') ? $('browserchart').hide() : null;
-	$('trendchart') ? $('trendchart').hide() : null;
-	$('usertable') ? $('usertable').hide() : null;
-	$('viewtable') ? $('viewtable').show() : null;
-	$('oschart') ? $('oschart').hide() : null;
-	$('uacchart') ? $('uacchart').hide() : null;
-	$('bovs') ? $('bovs').hide() : null;
-}
-
-trends = function() {
-	$('browserchart') ? $('browserchart').hide() : null;
-	$('trendchart') ? $('trendchart').show() : null;
-	$('usertable') ? $('usertable').hide() : null;
-	$('viewtable') ? $('viewtable').hide() : null;
-	$('oschart') ? $('oschart').hide() : null;
-	$('uacchart') ? $('uacchart').hide() : null;
-	$('bovs') ? $('bovs').hide() : null;
-}
-
-browsers = function() {
-	$('browserchart') ? $('browserchart').show() : null;
-	$('trendchart') ? $('trendchart').hide() : null;
-	$('usertable') ? $('usertable').hide() : null;
-	$('viewtable') ? $('viewtable').hide() : null;
-	$('oschart') ? $('oschart').hide() : null;
-	$('uacchart') ? $('uacchart').hide() : null;
-	$('bovs') ? $('bovs').hide() : null;
-}
-
-os = function() {
-	$('browserchart') ? $('browserchart').hide() : null;
-	$('trendchart') ? $('trendchart').hide() : null;
-	$('usertable') ? $('usertable').hide() : null;
-	$('viewtable') ? $('viewtable').hide() : null;
-	$('oschart') ? $('oschart').show() : null;
-	$('uacchart') ? $('uacchart').hide() : null;
-	$('bovs') ? $('bovs').hide() : null;
 }
 
 
 SiteTreeNode.prototype.onselect = function() {
 	switch(this.id) {
-		case 'statsroot' :
+		case 'statsroot':
 			break;
-		case 'stoverview' :
-			overview();
+		case 'stoverview':
+			StatisticsAdmin.overview();
 			break;
-		case 'stusers' :
-			users();
+		case 'stusers':
+			StatisticsAdmin.users();
 			break;
-		case 'stviews' :
-			views();
+		case 'stviews':
+			StatisticsAdmin.views();
 			break;
-		case 'sttrends' :
-			trends();
+		case 'sttrends':
+			StatisticsAdmin.trends();
 			break;
-		case 'stbrowsers' :
-			browsers();
+		case 'stbrowsers':
+			StatisticsAdmin.browsers();
 			break;
-		case 'stos' :
-			os();
+		case 'stos':
+			StatisticsAdmin.os();
 			break;
-		default :
+		default:
 			console.log('Unrecognized option ' + this.id);
 	}
 };
