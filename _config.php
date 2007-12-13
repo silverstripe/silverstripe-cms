e3583e1f736dcbf692c55404eff8f4a1a8adac40
Andrew O'Neil <andy@andyofniall.net>
2007-12-13 21:54:53 +0000

    Merged revisions 46633 via svnmerge from svn://svn.silverstripe.com/silverstripe/modules/cms/branches/2.2.0-privacy
    ........
      r46633 | aoneil | 2007-12-12 12:01:23 +1300 (Wed, 12 Dec 2007) | 1 line
      
      There is no MemberControlPanel - old custom code
    ........
    
    
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@46785 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- _config.php
+++ _config.php
@@ -17,8 +17,7 @@ Director::addRules(50, array(
 	'admin/bulkload/$Action/$ID/$OtherID' => 'BulkLoaderAdmin',
 	'admin/ImageEditor/$Action' => 'ImageEditor',
 	'admin/$Action/$ID/$OtherID' => 'CMSMain',
-	'unsubscribe/$Email/$MailingList' => 'Unsubscribe_Controller',
-	'membercontrolpanel/$Email' => 'MemberControlPanel'	
+	'unsubscribe/$Email/$MailingList' => 'Unsubscribe_Controller'
 ));
 
 ?>
