6546f66ebbd44f7cb05e9c475860dfd797ad4d17
Sam Minnee <sam@silverstripe.com>
2008-02-20 09:38:41 +0000

    Added deprecation note to BulkLoaderAdmin
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@49883 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/BulkLoaderAdmin.php
+++ code/BulkLoaderAdmin.php
@@ -10,9 +10,9 @@
  * The BulkLoaderAdmin class provides an interface for accessing all of the subclasses of BulkLoader,
  * each of which defines a particular bulk loading operation.
  * 
- * This code was originally developed for Per Week in collaboration with Brian Calhoun.
  * @package cms
  * @subpackage bulkloading
+ * @deprecated This class has never been used in production; if we're going to continue to support it we should increase its usefulness.
  */
 class BulkLoaderAdmin extends LeftAndMain {
 
