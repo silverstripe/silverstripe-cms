48bb2a55bb6978e27dd5130bbebda100702b8918
Ingo Schommer <ingo@silverstripe.com>
2008-03-12 09:23:10 +0000

    removed unused autocomplete code
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@50944 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/MemberTableField_popup.js
+++ javascript/MemberTableField_popup.js
@@ -4,36 +4,9 @@ MemberTableFieldPopupForm.prototype = {
 		this.ComplexTableFieldPopupForm.initialize();
 
 		Behaviour.register({
-			"form#MemberTableField_Popup_DetailForm input.action": {
+			"form#MemberTableField_Popup_DetailForm .Actions input.action": {
 				onclick: this.submitForm.bind(this)
-			},
-
-			'form#MemberTableField_Popup_DetailForm input' : {
-				initialise : function() {
-					if(this.name == 'FirstName' || this.name == 'Surname' || this.name == 'Email') {
-						var div = document.createElement('div');
-						div.id = this.id + '_ac';
-						div.className = 'autocomplete';
-						this.parentNode.appendChild(div);
-						/*
-						new Ajax.Autocompleter(this.id, div.id, 'admin/security/autocomplete/' + this.name, {
-							afterUpdateElement : this.afterAutocomplete.bind(this)
-						});
-						*/
-					}
-				},
-				afterAutocomplete : function(field, selectedItem) {
-					var data = selectedItem.getElementsByTagName('span')[1].innerHTML;
-					var items = data.split(",");
-
-					this.elements.FirstName.value = items[0];
-					this.elements.Surname.value = items[1];
-					this.elements.Email.value = items[2];
-					this.elements.Password.value = items[3];
-				}
 			}
-
-			//'form#MemberTableField_Popup_DetailForm input' : AjaxMemberLookup
 		});
 	}
 }
