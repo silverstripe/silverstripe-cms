155269c389c3c3c3e3e16f7b2941b3a221270a9c
Sam Minnee <sam@silverstripe.com>
2008-10-03 02:26:23 +0000

    Added tests for security group export
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@63582 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ tests/SecurityAdminTest.php
@@ -0,0 +1,35 @@
+<?php
+
+class SecurityAdminTest extends FunctionalTest {
+	static $fixture_file = 'cms/tests/CMSMainTest.yml';
+	
+	function testGroupExport() {
+		$this->session()->inst_set('loggedInAs', $this->idFromFixture('Member', 'admin'));
+		
+		/* First, open the applicable group */
+		$this->get('admin/security/getitem?ID=' . $this->idFromFixture('Group','admin'));
+		$this->assertRegExp('/<input[^>]+id="Form_EditForm_Title"[^>]+value="Administrators"[^>]*>/',$this->content());
+		
+		/* Then load the export page */
+		$this->get('admin/security//EditForm/field/Members/export');
+		
+		$this->assertRegExp('/"FirstName","Surname","Email"/', $this->content());
+		$this->assertRegExp('/"","","admin@example.com"/', $this->content());
+	}
+
+	function testEmptyGroupExport() {
+		$this->session()->inst_set('loggedInAs', $this->idFromFixture('Member', 'admin'));
+		
+		/* First, open the applicable group */
+		$this->get('admin/security/getitem?ID=' . $this->idFromFixture('Group','empty'));
+		$this->assertRegExp('/<input[^>]+id="Form_EditForm_Title"[^>]+value="Empty Group"[^>]*>/',$this->content());
+		
+		/* Then load the export page */
+		$this->get('admin/security//EditForm/field/Members/export');
+		
+		$this->assertRegExp('/"FirstName","Surname","Email"/', $this->content());
+	}
+	
+}
+
+?>
