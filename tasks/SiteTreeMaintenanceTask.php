d113768d7b0be31d33f439ebd00e3bc5ee8a3d33
Ingo Schommer <ingo@silverstripe.com>
2008-10-16 12:21:30 +0000

    FEATURE Added SiteTreeMaintenanceTask
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@64417 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ tasks/SiteTreeMaintenanceTask.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package cms
+ * @subpackage tasks
+ */
+class SiteTreeMaintenanceTask extends Task {
+	static $allowed_actions = array(
+		'*' => 'ADMIN'
+	);
+	
+	function makelinksunique() {
+		$badURLs = "'" . implode("', '", DB::query("SELECT URLSegment, count(*) FROM SiteTree GROUP BY URLSegment HAVING count(*) > 1")->column()) . "'";
+		$pages = DataObject::get("SiteTree", "URLSegment IN ($badURLs)");
+
+		foreach($pages as $page) {
+			echo "<li>$page->Title: ";
+			$urlSegment = $page->URLSegment;
+			$page->write();
+			if($urlSegment != $page->URLSegment) {
+				echo sprintf(_t('SiteTree.LINKSCHANGEDTO', " changed %s -> %s"), $urlSegment, $page->URLSegment);
+			}
+			else {
+				echo sprintf(_t('SiteTree.LINKSALREADYUNIQUE', " %s is already unique"), $urlSegment);
+			}
+			die();
+		}
+	}
+}
+?>
