4bba3151dfe3bd5b3c411cfd12b5868315b6623c
Will Rossiter <will@silverstripe.com>
2008-11-25 01:53:46 +0000

    MINOR: actually allow youtube / blip videos to be embedded into the cms content area
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.3@66542 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/tinymce.template.js
+++ javascript/tinymce.template.js
@@ -41,7 +41,7 @@ if((typeof tinyMCE != 'undefined')) {
 		relative_urls : true,
 		verify_html : true,
 		valid_elements : "+a[id|rel|rev|dir|tabindex|accesskey|type|name|href|target|title|class],-strong/-b[class],-em/-i[class],-strike[class],-u[class],#p[id|dir|class|align],-ol[class],-ul[class],-li[class],br,img[id|dir|longdesc|usemap|class|src|border|alt=|title|width|height|align],-sub[class],-sup[class],-blockquote[dir|class],-table[border=0|cellspacing|cellpadding|width|height|class|align|summary|dir|id|style],-tr[id|dir|class|rowspan|width|height|align|valign|bgcolor|background|bordercolor|style],tbody[id|class|style],thead[id|class|style],tfoot[id|class|style],-td[id|dir|class|colspan|rowspan|width|height|align|valign|scope|style],-th[id|dir|class|colspan|rowspan|width|height|align|valign|scope|style],caption[id|dir|class],-div[id|dir|class|align],-span[class|align],-pre[class|align],address[class|align],-h1[id|dir|class|align],-h2[id|dir|class|align],-h3[id|dir|class|align],-h4[id|dir|class|align],-h5[id|dir|class|align],-h6[id|dir|class|align],hr[class],dd[id|class|title|dir],dl[id|class|title|dir],dt[id|class|title|dir],iframe[align<bottom?left?middle?right?top|class|frameborder|height|id|longdesc|marginheight|marginwidth|name|scrolling<auto?no?yes|src|style|title|width]",
-		extended_valid_elements : "img[class|src|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],script[src|type],object[width|height|type|data],param[value|name]"
+		extended_valid_elements : "img[class|src|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],script[src|type],object[width|height|type|data|classid|codebase],param[value|name],embed[src|quality|wmode|pluginspage|type|width|height]"
 	});
 }
 
