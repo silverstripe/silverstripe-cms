811cdb66a31bcc3689131bceb4b912bdd6751b98
Sean Harvey <sean@silverstripe.com>
2008-11-26 23:59:05 +0000

    BUGFIX Fixed "Show specification..." link not working because ModelName had spaces in it
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.3@66791 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/ModelAdmin.php
+++ code/ModelAdmin.php
@@ -374,7 +374,7 @@ class ModelAdmin_CollectionController extends Controller {
 			$specRelations->push(new ArrayData(array('Name' => $name, 'Description' => $desc)));
 		}
 		$specHTML = $this->customise(array(
-			'ModelName' => singleton($modelName)->i18n_singular_name(), 
+			'ModelName' => str_replace(' ', '', singleton($modelName)->i18n_singular_name()), 
 			'Fields' => $specFields,
 			'Relations' => $specRelations, 
 		))->renderWith('ModelAdmin_ImportSpec');
