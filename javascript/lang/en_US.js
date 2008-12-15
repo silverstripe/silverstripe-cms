f4f02b7afcd270b940d2ca58e178ba53f9fd14e3
Sean Harvey <sean@silverstripe.com>
2008-12-15 01:19:08 +0000

    BUGFIX "console not defined" error in IE in en_US.js, check typeof(console) is not undefined before calling console.error()
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.3@68890 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/lang/en_US.js
+++ javascript/lang/en_US.js
@@ -1,5 +1,5 @@
 if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
-	console.error('Class ss.i18n not defined');
+	if(typeof(console) != 'undefined') console.error('Class ss.i18n not defined');
 } else {
 	ss.i18n.addDictionary('en_US', {
 		'CMSMAIN.WARNINGSAVEPAGESBEFOREADDING' : "You have to save a page before adding children underneath it",
