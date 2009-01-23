b686f45ab5ac91903ae70c459bb18d0467e15485
Ingo Schommer <ingo@silverstripe.com>
2009-01-23 03:10:23 +0000

    MINOR Removed hard-to-debug error trap in SecurityAdmin_right.js
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.3@70677 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/SecurityAdmin_right.js
+++ javascript/SecurityAdmin_right.js
@@ -5,7 +5,5 @@ function action_addmember_right() {
 	var memberTableFields = document.getElementsBySelector('#Form_EditForm div.MemberTableField');
 	var tables = document.getElementsBySelector('#Form_EditForm div.MemberTableField table');
 	var addLinks = document.getElementsBySelector('#Form_EditForm div.MemberTableField a.addlink');
-	try {
-		memberTableFields[0].openPopup(null,addLinks[0].href,tables[0]);
-	} catch(err) {}
+	memberTableFields[0].openPopup(null,addLinks[0].href,tables[0]);
 }
