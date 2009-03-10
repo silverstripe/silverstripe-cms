de375ae0a2a43dfa4f8c743199734d8932ebf76d
Andrew O'Neil <andy@andyofniall.net>
2009-03-10 22:33:29 +0000

    BUGFIX: #3645 - CommenterURL missing from CMS
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.3@72816 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/CommentAdmin.php
+++ code/CommentAdmin.php
@@ -70,12 +70,14 @@ class CommentAdmin extends LeftAndMain {
 		$tableFields = array(
 			"Name" => _t('CommentAdmin.AUTHOR', 'Author'),
 			"Comment" => _t('CommentAdmin.COMMENT', 'Comment'),
+			"CommenterURL" => _t('CommentAdmin.COMMENTERURL', 'URL'),
 			"PageTitle" => _t('CommentAdmin.PAGE', 'Page'),
 			"Created" => _t('CommentAdmin.DATEPOSTED', 'Date Posted')
 		);	
 		
 		$popupFields = new FieldSet(
 			new TextField('Name', _t('CommentAdmin.NAME', 'Name')),
+			new TextField('CommenterURL', _t('CommentAdmin.COMMENTERURL', 'URL')),
 			new TextareaField('Comment', _t('CommentAdmin.COMMENT', 'Comment'))
 		);
 		
