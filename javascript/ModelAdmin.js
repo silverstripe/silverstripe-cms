d1c52def2ff90bc1b6f7c66c99c5fe3eee08e836
Sam Minnee <sam@silverstripe.com>
2009-03-20 03:28:00 +0000

    Fixed issue with ModelAdmin tab CSS
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.3@73452 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/ModelAdmin.js
+++ javascript/ModelAdmin.js
@@ -22,7 +22,13 @@ $(document).ready(function() {
 	 * Attach tabs plugin to the set of search filter and edit forms
 	 */
 	$('ul.tabstrip').livequery(function() {
-	    $(this).tabs();
+	    $(this).tabs({
+			// This show handler is necessary to get tabs working properly with the crappy old layout_helpers.js layout
+			// manager
+			show : function() {
+				if(window.onresize) window.onresize();
+			}
+		});
 	});
 
     /*
