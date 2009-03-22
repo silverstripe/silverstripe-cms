405f698b34ca72f02a10d80eb6d4c1e781ce3da4
sminnee <sminnee@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-03-22 04:41:36 +0000

    Make Page.php compatible with asp_tags setting
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/phpinstaller/trunk@73488 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -15,8 +15,8 @@ class Page_Controller extends ContentController {
 	public function init() {
 		parent::init();
 
-		// Note: you should use <% require %> tags inside your templates instead of putting Requirements calls here.  However
-		// these are included so that our older themes still work
+		/* Note: you should use <% require %> tags inside your templates instead of putting Requirements calls here.  However
+		 * these are included so that our older themes still work */
 		Requirements::themedCSS("layout"); 
 		Requirements::themedCSS("typography"); 
 		Requirements::themedCSS("form"); 
