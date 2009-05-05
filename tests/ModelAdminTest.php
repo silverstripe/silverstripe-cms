5f8839b6d8c50452465b270561350162beb14ec5
Sam Minnee <sam@silverstripe.com>
2009-05-05 22:12:38 +0000

    MINOR: Added additional tests for LeftAndMain and ModelAdmin
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.3@76112 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ tests/ModelAdminTest.php
@@ -0,0 +1,29 @@
+<?php
+
+class ModelAdminTest extends FunctionalTest {
+	static $fixture_file = 'cms/tests/ModelAdminTest.yml';
+	
+	function testModelAdminOpens() {
+		$this->autoFollowRedirection = false;
+		$this->logInAs('admin');
+		$this->assertTrue((bool)Permission::check("ADMIN"));
+		
+		Debug::message($this->get('ModelAdminTest_Admin')->getBody());
+		$this->assertEquals(200, $this->get('ModelAdminTest_Admin')->getStatusCode());
+	}
+}
+
+class ModelAdminTest_Admin extends ModelAdmin {
+	static $url_segment = 'testadmin';
+	
+	protected static $managed_models = array(
+		'ModelAdminTest_Contact',
+	);
+}
+
+class ModelAdminTest_Contact extends DataObject {
+	static $db = array(
+		"Name" => "Varchar",
+		"Phone" => "Varchar",
+	);
+}
