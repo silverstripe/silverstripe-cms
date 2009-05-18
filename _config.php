8625e3bce300a09f3dbbcf03782d2f99577d9f19
Sean Harvey <sean@silverstripe.com>
2009-05-18 05:31:22 +0000

    Merged from branches/2.3
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@77067 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- _config.php
+++ _config.php
@@ -18,36 +18,4 @@ Director::addRules(50, array(
 
 CMSMenu::populate_menu();
 
-HtmlEditorConfig::get('cms')->setOptions(array(
-	'mode' => 'none',
-	'language' => i18n::get_tinymce_lang(),
-	'content_css' => 'cms/css/editor.css, '.(SSViewer::current_theme() ? THEMES_DIR . "/" . SSViewer::current_theme() : project()) . "/css/editor.css",
-
-	'body_class' => 'typography',
-	'document_base_url' => Director::absoluteBaseURL(),
-
-	'urlconverter_callback' => "nullConverter",
-	'setupcontent_callback' => "sapphiremce_setupcontent",
-	'cleanup_callback' => "sapphiremce_cleanup",
-
-	'template_templates' => array(
-    	array( 'title' => "Three column", 'src' => "assets/snippet.html", 'description' => "A simple 3 column layout" )
-	),
-
-	'use_native_selects' => true, // fancy selects are bug as of SS 2.3.0
-	'valid_elements' => "+a[id|rel|rev|dir|tabindex|accesskey|type|name|href|target|title|class],-strong/-b[class],-em/-i[class],-strike[class],-u[class],#p[id|dir|class|align],-ol[class],-ul[class],-li[class],br,img[id|dir|longdesc|usemap|class|src|border|alt=|title|width|height|align],-sub[class],-sup[class],-blockquote[dir|class],-table[border=0|cellspacing|cellpadding|width|height|class|align|summary|dir|id|style],-tr[id|dir|class|rowspan|width|height|align|valign|bgcolor|background|bordercolor|style],tbody[id|class|style],thead[id|class|style],tfoot[id|class|style],-td[id|dir|class|colspan|rowspan|width|height|align|valign|scope|style],-th[id|dir|class|colspan|rowspan|width|height|align|valign|scope|style],caption[id|dir|class],-div[id|dir|class|align|style],-span[class|align],-pre[class|align],address[class|align],-h1[id|dir|class|align],-h2[id|dir|class|align],-h3[id|dir|class|align],-h4[id|dir|class|align],-h5[id|dir|class|align],-h6[id|dir|class|align],hr[class],dd[id|class|title|dir],dl[id|class|title|dir],dt[id|class|title|dir]",
-	'extended_valid_elements' => "img[class|src|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|usemap],iframe[src|name|width|height|align|frameborder|marginwidth|marginheight|scrolling],object[width|height|data|type],param[name|value],map[class|name|id],area[shape|coords|href|target|alt]"
-));
-			
-
-HtmlEditorConfig::get('cms')->disablePlugins('blockquote');
-HtmlEditorConfig::get('cms')->enablePlugins('media', '../../tinymce_ssbuttons', 'fullscreen');
-			
-HtmlEditorConfig::get('cms')->insertButtonsBefore('formatselect', 'styleselect');
-HtmlEditorConfig::get('cms')->insertButtonsBefore('advcode', 'ssimage', 'ssflash', 'sslink', 'unlink', 'anchor', 'separator' );
-HtmlEditorConfig::get('cms')->insertButtonsAfter ('advcode', 'fullscreen', 'separator');
-			
-HtmlEditorConfig::get('cms')->removeButtons('tablecontrols');
-HtmlEditorConfig::get('cms')->addButtonsToLine(3, 'tablecontrols');
-
 ?>
