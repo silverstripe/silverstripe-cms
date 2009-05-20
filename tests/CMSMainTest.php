60200f1aa37dd0781e103cf226a53f733860ea3e
Sean Harvey <sean@silverstripe.com>
2009-05-20 07:09:54 +0000

    MINOR Removed unsetting of GhostPage from CMSMainTest - these are deleted in r77383
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@77389 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- tests/CMSMainTest.php
+++ tests/CMSMainTest.php
@@ -47,7 +47,6 @@ class CMSMainTest extends FunctionalTest {
 		return;
 		$classes = ClassInfo::subclassesFor("SiteTree");
 		array_shift($classes);
-		unset($classes['GhostPage']); //Ghost Pages aren't used anymore
 
 		foreach($classes as $class) {
 			$page = new $class();
@@ -73,7 +72,6 @@ class CMSMainTest extends FunctionalTest {
 	function testThatGetCMSFieldsWorksOnEveryPageType() {
 		$classes = ClassInfo::subclassesFor("SiteTree");
 		array_shift($classes);
-		unset($classes['GhostPage']); //Ghost Pages aren't used anymore
 
 		foreach($classes as $class) {
 			$page = new $class();
