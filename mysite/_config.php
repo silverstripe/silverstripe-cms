8e9ae1d26ce4543e3704594fef61200db5e2faa0
sminnee <sminnee@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-06-27 13:26:54 +0000

    ENHANCEMENT: Set connection charset to utf8 for new sites.
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/phpinstaller/trunk@80232 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- mysite/_config.php
+++ mysite/_config.php
@@ -8,6 +8,8 @@ $database = "";
 
 require_once("conf/ConfigureFromEnv.php");
 
+MySQLDatabase::set_connection_charset('utf8');
+
 // This line set's the current theme. More themes can be
 // downloaded from http://www.silverstripe.com/themes/
 SSViewer::set_theme('blackcandy');
