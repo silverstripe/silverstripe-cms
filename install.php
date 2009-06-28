64628f99aebb735d982f050bb052194ad7974eaa
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-06-28 20:37:22 +0000

    BUGFIX Sett connection charset to utf8 for installer.php (see r80232)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/phpinstaller/trunk@80261 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- install.php
+++ install.php
@@ -661,6 +661,8 @@ global \$database;
 
 require_once("conf/ConfigureFromEnv.php");
 
+MySQLDatabase::set_connection_charset('utf8');
+
 // This line set's the current theme. More themes can be
 // downloaded from http://www.silverstripe.com/themes/
 SSViewer::set_theme('$theme');
