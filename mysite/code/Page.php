6815399bc548a17a8b1a88a42651b814c49df3df
sminnee <sminnee@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-10-11 00:47:05 +0000

    ENHANCEMENT: Include $allowed_actions by default on new sites, to encourage people to build more secure sites.
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/phpinstaller/trunk@88520 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -12,6 +12,25 @@ class Page extends SiteTree {
 
 class Page_Controller extends ContentController {
 	
+	/**
+	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
+	 * permissions or conditions required to allow the user to access it.
+	 *
+	 * <code>
+	 * array (
+	 *     'action', // anyone can access this action
+	 *     'action' => true, // same as above
+	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
+	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
+	 * );
+	 * </code>
+	 *
+	 * @var array
+	 */
+	public static $allowed_actions = array (
+		'SearchForm'
+	);
+	
 	public function init() {
 		parent::init();
 
