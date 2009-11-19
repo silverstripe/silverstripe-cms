ed24d088553380a0952c20e0a93a37b727d856d8
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-11-19 20:33:47 +0000

    BUGFIX: Allow SilverStripe to work when asp_tags is on (from r72337) (from r92239)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/phpinstaller/branches/2.4@92275 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -34,8 +34,9 @@ class Page_Controller extends ContentController {
 	public function init() {
 		parent::init();
 
-		/* Note: you should use <% require %> tags inside your templates instead of putting Requirements calls here.  However
-		 * these are included so that our older themes still work */
+		// Note: you should use SS template require tags inside your templates 
+		// instead of putting Requirements calls here.  However these are 
+		// included so that our older themes still work
 		Requirements::themedCSS("layout"); 
 		Requirements::themedCSS("typography"); 
 		Requirements::themedCSS("form"); 
