f297ec9ac2f8169d765661d73ec1d0a88c3f9f20
Ingo Schommer <ingo@silverstripe.com>
2009-11-21 02:38:08 +0000

    ENHANCEMENT Removed a.cmsEditLink javascript behaviour, do a full page load as normal links instead
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@92645 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/CMSMain_right.js
+++ javascript/CMSMain_right.js
@@ -1,15 +1,4 @@
 Behaviour.register({
-	'a.cmsEditlink' : {
-		onclick : function() {
-			if(this.href.match(/admin\/show\/([0-9]+)($|#|\?)/)) {
-				$('Form_EditForm').getPageFromServer(RegExp.$1);
-				return false;
-			}
-		}
-	}
-});
-
-Behaviour.register({
 	'#Form_EditForm' : {	
 		changeDetection_fieldsToIgnore : {
 			'restricted-chars[Form_EditForm_URLSegment]' : true,
