6aae0aaf53f875e8bb1c904f23cd6934e3d36855
Ingo Schommer <ingo@silverstripe.com>
2009-11-21 03:19:27 +0000

    MINOR Moved sitetree state setting javascript to LeftAndMain.Tree.js, used in AssetAdmin as well
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@92811 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/LeftAndMain.Tree.js
+++ javascript/LeftAndMain.Tree.js
@@ -16,6 +16,21 @@ var _HANDLER_FORMS = {
 	search : 'search_options'
 };
 
+(function($) {
+	/**
+	 * @class Tree panel.
+	 * @name ss.sitetree
+	 */
+	$('#sitetree').concrete('ss', function($){
+		return/** @lends ss.sitetree */{
+			onmatch: function() {
+				// make sure current ID of loaded form is actually selected in tree
+				var id = $('#Form_EditForm :input[name=ID]').val();
+				if(id) this[0].setCurrentByIdx(id);
+			}
+		};
+	});
+}(jQuery));
 
 /**
  * Overload this with a real context menu if necessary
