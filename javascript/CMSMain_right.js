4082561e5d7427c2223d9c354a093ab7a08d45bd
Ingo Schommer <ingo@silverstripe.com>
2009-11-21 02:38:01 +0000

    API CHANGE Removed flashColor(), stopFlashing(), suggestStageLink() javascript methods, they were never used
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@92643 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/CMSMain_right.js
+++ javascript/CMSMain_right.js
@@ -1,19 +1,3 @@
-
-
-function suggestStageSiteLink() {
-	var el = $('viewStageSite');
-	el.flasher = setInterval(flashColor.bind(el), 300);
-	setTimeout(stopFlashing.bind(el), 3000);
-}
-function flashColor() {
-	if(!this.style.color) this.style.color = '';
-	this.style.color =  (this.style.color == '') ? '#00FF00' : '';
-}
-function stopFlashing() {
-	clearInterval(this.flasher);
-}
-
-
 Behaviour.register({
 	'a.cmsEditlink' : {
 		onclick : function() {
