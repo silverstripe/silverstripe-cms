844acb217d71ab4052e99bcd385a23996d3506e7
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-11-21 00:42:47 +0000

    MINOR: Updating mergeinfo to ignore mergebacks
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/phpinstaller/trunk@92439 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- mysite/_config.php
+++ mysite/_config.php
@@ -4,15 +4,16 @@ global $project;
 $project = 'mysite';
 
 global $database;
-$database = "";
+$database = "SS_phpinstaller_trunk";
 
 require_once("conf/ConfigureFromEnv.php");
 
 MySQLDatabase::set_connection_charset('utf8');
 
 // This line set's the current theme. More themes can be
-// downloaded from http://www.silverstripe.com/themes/
+// downloaded from http://www.silverstripe.org/themes/
 SSViewer::set_theme('blackcandy');
 
 // enable nested URLs for this site (e.g. page/sub-page/)
 SiteTree::enable_nested_urls();
+?>
