ed6334ddeefc009c76903ad794324d61b4742c57
Ingo Schommer <ingo@silverstripe.com>
2009-12-02 03:38:44 +0000

    MINOR Added pl_PL.js translation (#4788, thanks qlex)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@94224 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ javascript/lang/pl_PL.js
@@ -0,0 +1,27 @@
+if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
+	console.error('Class ss.i18n not defined');
+} else {
+	ss.i18n.addDictionary('pl_PL', {
+		'CMSMAIN.WARNINGSAVEPAGESBEFOREADDING' : "Należy najpierw zapisać stronę, aby móc dodać strony podrzędne",
+		'CMSMAIN.CANTADDCHILDREN' : "Nie można dodać stron podrzędnych",
+		'CMSMAIN.ERRORADDINGPAGE' : 'Błąd przy dodawaniu strony',
+		'CMSMAIN.FILTEREDTREE' : 'Drzewo filtrowane pokazujące tylko zmienione strony',
+		'CMSMAIN.ERRORFILTERPAGES' : 'Nie można było filtrować drzewa<br />%s',
+		'CMSMAIN.ERRORUNFILTER' : 'Nie można było cofnąć filtrowania drzewa<br />%s',
+		'CMSMAIN.ERRORUNFILTER' : 'Cofnięto filtrowanie drzewa',
+		'CMSMAIN.PUBLISHINGPAGES' : 'Publikacja strony...',
+		'CMSMAIN.SELECTONEPAGE' : "Proszę wybrać przynajmniej jedną stronę",
+		'CMSMAIN.ERRORPUBLISHING' : 'Błąd podczas publikacji stron',
+		'CMSMAIN.REALLYDELETEPAGES' : "Czy na pewno zaznaczone strony %s usunąć?",
+		'CMSMAIN.DELETINGPAGES' : 'Usuwanie stron...',
+		'CMSMAIN.ERRORDELETINGPAGES': 'Błąd podczas usuwania stron',
+		'CMSMAIN.PUBLISHING' : 'Publikacja...',
+		'CMSMAIN.RESTORING': 'Odzyskiwanie...',
+		'CMSMAIN.ERRORREVERTING': 'Błąd podczas powrotu do opublikowanej strony',
+		'CMSMAIN.SAVING' : 'Zapisywanie...',
+		'ModelAdmin.SAVED': "Zapisano",
+		'ModelAdmin.REALLYDELETE': "Napewno usunąć?",
+		'ModelAdmin.DELETED': "Usunięto",
+		'LeftAndMain.PAGEWASDELETED': "Ta strona została usunięta."
+	});
+}
