fd1a4dde052ac1e83b3bcc4d8c5dd470f387c1e5
sminnee <sminnee@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-01-26 09:09:43 +0000

    MINOR: Accidental commit
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/phpinstaller/branches/2.4@97590 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- mysite/_config.php
+++ mysite/_config.php
@@ -10,6 +10,18 @@ require_once("conf/ConfigureFromEnv.php");
 
 MySQLDatabase::set_connection_charset('utf8');
 
+// Use SQLite on this project
+/*
+global $databaseConfig;
+$databaseConfig = array(
+	"type" => "SQLite3Database",
+	"memory" => true, // only used for testing
+	"path" => ASSETS_PATH,
+	"database" => "data.sqlite",
+	
+);
+*/
+
 // This line set's the current theme. More themes can be
 // downloaded from http://www.silverstripe.com/themes/
 SSViewer::set_theme('blackcandy');
