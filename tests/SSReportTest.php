4982aac955d0ccf30f8280666e3bb8aab4cc46e5
Sam Minnee <sam@silverstripe.com>
2010-02-04 04:53:16 +0000

    MINOR remove sort direction when running canSortBy. Also added test coverage for this. (from r96428)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.4@98192 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ tests/SSReportTest.php
@@ -0,0 +1,27 @@
+<?php
+
+class SSReportTest extends SapphireTest {
+	
+	function testCanSortBy() {
+		$report = new SSReportTest_FakeTest();
+		$this->assertTrue($report->sourceQuery(array())->canSortBy('Title ASC'));
+		$this->assertTrue($report->sourceQuery(array())->canSortBy('Title DESC'));
+		$this->assertTrue($report->sourceQuery(array())->canSortBy('Title'));
+	}
+}
+
+class SSReportTest_FakeTest extends SSReport implements TestOnly {
+	function title() {
+		return 'Report title';
+	}
+	function columns() {
+		return array(
+			"Title" => array(
+				"title" => "Page Title"
+			)
+		);
+	}
+	function sourceRecords($params, $sort, $limit) {
+		return new DataObjectSet();
+	}
+}
