c941ca899aba43936e35918266440ff72c98fb53
Ingo Schommer <ingo@silverstripe.com>
2010-02-16 04:14:40 +0000

    MINOR Fixed merge error (see r99114)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.4@99130 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/CMSMain.php
+++ code/CMSMain.php
@@ -120,7 +120,7 @@ class CMSMain extends LeftAndMain implements CurrentPageIdentifier, PermissionPr
 	
 	function SwitchView() {
 		if($page = $this->currentPage()) {
-			$nav = $page->getNavigatorItems();
+			$nav = SilverStripeNavigator::get_for_record($page);
 			Requirements::customScript("window.name = windowName('cms');");
 			return $nav['items'];
 		}
