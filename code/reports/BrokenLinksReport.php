82f95ae415b8c81d94bf2ab5622c8590d52b31f9
Ingo Schommer <ingo@silverstripe.com>
2010-02-16 04:02:14 +0000

    MINOR fixed two reports with links that did not open in new windows (from r97816)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.4@99118 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/reports/BrokenLinksReport.php
+++ code/reports/BrokenLinksReport.php
@@ -86,7 +86,7 @@ class BrokenLinksReport extends SS_Report {
 			),
 			'AbsoluteLink' => array(
 				'title' => 'URL',
-				'formatting' => '$value " . ($AbsoluteLiveLink ? "<a href=\"$AbsoluteLiveLink\">(live)</a>" : "") . " <a href=\"$value?stage=Stage\">(draft)</a>'
+				'formatting' => '$value " . ($AbsoluteLiveLink ? "<a target=\"_blank\" href=\"$AbsoluteLiveLink\">(live)</a>" : "") . " <a target=\"_blank\" href=\"$value?stage=Stage\">(draft)</a>'
 			)
 		);
 		
