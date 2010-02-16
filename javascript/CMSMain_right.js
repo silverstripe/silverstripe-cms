686f8a45dc6933092b9b71a34669a0c1a9efb61c
Ingo Schommer <ingo@silverstripe.com>
2010-02-16 04:02:53 +0000

    MINOR added LiveURLSegment to ignored field in change detection (from r98494)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.4@99120 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/CMSMain_right.js
+++ javascript/CMSMain_right.js
@@ -98,7 +98,8 @@ Behaviour.register({
 	'#Form_EditForm' : {	
 		changeDetection_fieldsToIgnore : {
 			'restricted-chars[Form_EditForm_URLSegment]' : true,
-			'Sort' : true	
+			'Sort' : true,
+			'LiveURLSegment' : true
 		}
 	},
 
