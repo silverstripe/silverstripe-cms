silverstripe/silverstripe-cms

commit 7e6dfe14f55bfc54351c339895649bf323e77097

BUGFIX Fixed MemberTableField limiting of , wasnt taking children groups into account (regression from r99684)
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@99706 467b73ca-7a2a-4603-9d3b-597d59a354a9

code/MemberTableField.php
@@ -103,7 +103,10 @@ class MemberTableField extends ComplexTableField {
 		}
 		
 		if($this->group) {
-			$this->sourceFilter[] = sprintf('"Group_Members"."GroupID" = %d', $this->group->ID);
+			$this->sourceFilter[] = sprintf(
+				'"Group_Members"."GroupID" IN (%s)', 
+				implode(',', array_merge($this->group->AllChildren()->column('ID'), array($this->group->ID)))
+			);
 		}
 
 		$this->sourceJoin = " INNER JOIN \"Group_Members\" ON \"MemberID\"=\"Member\".\"ID\"";

commit 9c75acb1134772f9ccadd39bf1ea76fbb046905d

MINOR Fixed MemberTableField regression from r99706
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@99710 467b73ca-7a2a-4603-9d3b-597d59a354a9

code/MemberTableField.php
@@ -103,9 +103,11 @@ class MemberTableField extends ComplexTableField {
 		}
 		
 		if($this->group) {
+			$groupIDs = array($this->group->ID);
+			if($this->group->AllChildren()) $groupIDs = array_merge($groupIDs, $this->group->AllChildren()->column('ID'));
 			$this->sourceFilter[] = sprintf(
 				'"Group_Members"."GroupID" IN (%s)', 
-				implode(',', array_merge($this->group->AllChildren()->column('ID'), array($this->group->ID)))
+				implode(',', $groupIDs)
 			);
 		}
 