d8736b938659d7d49af2a38b379718504ccd36bb
Ingo Schommer <ingo@silverstripe.com>
2010-03-11 04:37:29 +0000

    MINOR Using jquery.live instead of livequery in MemberImportForm.js
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.4@100888 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/MemberImportForm.js
+++ javascript/MemberImportForm.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 	$('.import-form .advanced').hide();
 	
-	$('.import-form a.toggle-advanced').livequery(
+	$('.import-form a.toggle-advanced').live(
 		'click', 
 		function(e) {
 			$(this).parents('form:eq(0)').find('.advanced').toggle();
