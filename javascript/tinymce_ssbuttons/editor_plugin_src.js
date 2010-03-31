77718a05f018012eea33c1fa5cb41dcd4bfbc14c
Ingo Schommer <ingo@silverstripe.com>
2010-03-31 05:35:03 +0000

    MINOR Fixed indentation
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.4@101971 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/tinymce_ssbuttons/editor_plugin_src.js
+++ javascript/tinymce_ssbuttons/editor_plugin_src.js
@@ -20,74 +20,73 @@
 			};
 		},
 
-
 		init : function(ed, url) {
-	    	/**
-	    	 * These map the action buttons to the IDs of the forms that they open/close
-	    	 */
-	    	forms = {
-	    	    'sslink' : 'Form_EditorToolbarLinkForm',
-	    	    'ssimage' : 'Form_EditorToolbarImageForm',
-	    	    'ssflash' : 'Form_EditorToolbarFlashForm'
-	    	};
+			/**
+			 * These map the action buttons to the IDs of the forms that they open/close
+			 */
+			forms = {
+				'sslink' : 'Form_EditorToolbarLinkForm',
+				'ssimage' : 'Form_EditorToolbarImageForm',
+				'ssflash' : 'Form_EditorToolbarFlashForm'
+			};
 
-	        ed.addButton('sslink', {title : ed.getLang('tinymce_ssbuttons.insertlink'), cmd : 'sslink', 'class' : 'mce_link'}); 
-	        ed.addButton('ssimage', {title : ed.getLang('tinymce_ssbuttons.insertimage'), cmd : 'ssimage', 'class' : 'mce_image'}); 
-	        ed.addButton('ssflash', {title : ed.getLang('tinymce_ssbuttons.insertflash'), cmd : 'ssflash', 'class' : 'mce_flash', 'image': url + '/img/flash.gif'}); 
+			ed.addButton('sslink', {title : ed.getLang('tinymce_ssbuttons.insertlink'), cmd : 'sslink', 'class' : 'mce_link'}); 
+			ed.addButton('ssimage', {title : ed.getLang('tinymce_ssbuttons.insertimage'), cmd : 'ssimage', 'class' : 'mce_image'}); 
+			ed.addButton('ssflash', {title : ed.getLang('tinymce_ssbuttons.insertflash'), cmd : 'ssflash', 'class' : 'mce_flash', 'image': url + '/img/flash.gif'}); 
 
-            /**
-             * Show a side panel, hiding others
-             * If showCommand isn't set, then this will simply hide panels
-             */
-	    	function showSidePanel(showCommand, hideCommands) {
-	    	    hideCommands.each(function(command) { 
-	    	        ed.controlManager.setActive(command,false);
-	    	        Element.hide(forms[command]); 
-	    	    });
+			/**
+			 * Show a side panel, hiding others
+			 * If showCommand isn't set, then this will simply hide panels
+			 */
+			function showSidePanel(showCommand, hideCommands) {
+				hideCommands.each(function(command) { 
+					ed.controlManager.setActive(command,false);
+					Element.hide(forms[command]); 
+				});
 
-                var showForm = null;
-                if(forms[showCommand]) {
-                    showForm = $(forms[showCommand]);
-                    showForm.toggle(ed);
-                }
+				var showForm = null;
+				if(forms[showCommand]) {
+					showForm = $(forms[showCommand]);
+					showForm.toggle(ed);
+				}
 
-                if(!showForm || showForm.style.display == "none") {
-	        		ed.controlManager.setActive(showCommand, false);
-	        		// Can't use $('contentPanel'), as its in a different window
-	        		window.parent.document.getElementById('contentPanel').style.display = "none";
-	    		} else {
-	        		ed.controlManager.setActive(showCommand, true);
-	        		window.parent.document.getElementById('contentPanel').style.display = "block";
-	    		}
-	    		window.onresize();
-	    	}
+				if(!showForm || showForm.style.display == "none") {
+					ed.controlManager.setActive(showCommand, false);
+					// Can't use $('contentPanel'), as its in a different window
+					window.parent.document.getElementById('contentPanel').style.display = "none";
+				} else {
+					ed.controlManager.setActive(showCommand, true);
+					window.parent.document.getElementById('contentPanel').style.display = "block";
+				}
+				window.onresize();
+			}
 
-            ed.addCommand("ssclosesidepanel", function(ed) {
-                showSidePanel('', [ 'sslink', 'ssimage', 'ssflash' ]);
-            });
+			ed.addCommand("ssclosesidepanel", function(ed) {
+				showSidePanel('', [ 'sslink', 'ssimage', 'ssflash' ]);
+			});
 
-		    ed.addCommand("sslink", function(ed) {
-		        showSidePanel('sslink', [ 'ssimage', 'ssflash' ]);
+			ed.addCommand("sslink", function(ed) {
+				showSidePanel('sslink', [ 'ssimage', 'ssflash' ]);
 			});
 
 			ed.addCommand("ssimage", function(ed) {
-		        showSidePanel('ssimage', [ 'sslink', 'ssflash' ]);
+				showSidePanel('ssimage', [ 'sslink', 'ssflash' ]);
 			});
 
 			ed.addCommand("ssflash", function(ed) {
-		        showSidePanel('ssflash', [ 'ssimage', 'sslink' ]);
+				showSidePanel('ssflash', [ 'ssimage', 'sslink' ]);
 			});
 
 			ed.onNodeChange.add(function(ed, o) {
-	    	    if ($('Form_EditorToolbarLinkForm').updateSelection) {
-			    $('Form_EditorToolbarLinkForm').updateSelection(ed);
-			    $('Form_EditorToolbarLinkForm').respondToNodeChange(ed);
-		    }
-		    $('Form_EditorToolbarImageForm').respondToNodeChange(ed);
-	        });
-	        ed.onKeyUp.add(function(ed, o) {
-	    	    $('Form_EditorToolbarLinkForm').updateSelection(ed);
-		    });
+				if ($('Form_EditorToolbarLinkForm').updateSelection) {
+					$('Form_EditorToolbarLinkForm').updateSelection(ed);
+					$('Form_EditorToolbarLinkForm').respondToNodeChange(ed);
+				}
+				$('Form_EditorToolbarImageForm').respondToNodeChange(ed);
+			});
+			ed.onKeyUp.add(function(ed, o) {
+				$('Form_EditorToolbarLinkForm').updateSelection(ed);
+			});
 		
 			// resize image containers when the image is resized.
 			if(!tinymce.isOpera && !tinymce.isWebKit) ed.onMouseUp.add(function(ed, o) {
@@ -96,8 +95,8 @@
 					// we have to delay the resize check here, as this event handler is called before the actual image
 					// resizing is done.
 					setTimeout(function() {
-						var ed        = tinyMCE.activeEditor, // we need to redeclare these for IE.
-							node      = ed.selection.getNode(),
+						var ed		= tinyMCE.activeEditor, // we need to redeclare these for IE.
+							node	  = ed.selection.getNode(),
 							container = ed.dom.getParent(node, 'div');
 
 						if(node.width && node.width != parseInt(ed.dom.getStyle(container, 'width'))) {
