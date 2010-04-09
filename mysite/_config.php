e8db5c30c173b30bb31b5a6ea649190c5548c830
phalkunz <phalkunz@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-04-09 04:58:59 +0000

    MINOR: revert -r102275
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/phpinstaller/branches/2.4@102276 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- mysite/_config.php
+++ mysite/_config.php
@@ -4,7 +4,7 @@ global $project;
 $project = 'mysite';
 
 global $database;
-$database = 'SS_ss24';
+$database = '';
 
 require_once('conf/ConfigureFromEnv.php');
 
@@ -14,13 +14,5 @@ MySQLDatabase::set_connection_charset('utf8');
 // downloaded from http://www.silverstripe.org/themes/
 SSViewer::set_theme('blackcandy');
 
-// Set the site locale
-i18n::set_locale('en_US');
-
 // enable nested URLs for this site (e.g. page/sub-page/)
 SiteTree::enable_nested_urls();
-
-// Mollom config 
-Mollom::setPublicKey("1819023dfcb10a667d10bd1578c5f39b");
-Mollom::setPrivateKey("2430fcbd65756ac6732a54a7c9ada116");
-SpamProtectorManager::set_spam_protector('MollomSpamProtector');
