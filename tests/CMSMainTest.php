c9df94a7af1f63ad5e526cce877e855c75b0a793
Ingo Schommer <ingo@silverstripe.com>
2010-04-13 06:57:30 +0000

    MINOR: Fixed CMSMainTest to be more flexible about modules altering the buttons. (from r96827)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@102702 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- tests/CMSMainTest.php
+++ tests/CMSMainTest.php
@@ -128,7 +128,7 @@ class CMSMainTest extends FunctionalTest {
 		$this->assertTrue($livePage->canDelete());
 
 		// Check that the 'restore' button exists as a simple way of checking that the correct page is returned.
-		$this->assertRegExp('/<input[^>]+type="submit"[^>]+name="action_restore"/i', $response->getBody());
+		$this->assertRegExp('/<input[^>]+type="submit"[^>]+name="action_(restore|revert)"/i', $response->getBody());
 	}
 	
 	/**
