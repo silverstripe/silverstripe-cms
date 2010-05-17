fc19125698273bce401dcd3229d4be1bec602484
Ingo Schommer <ingo@silverstripe.com>
2010-05-17 03:04:26 +0000

    API CHANGE Removed ImageEditor functionality, please use thirdparty modules, e.g. "silverstripe-pixlr" (http://github.com/nyeholt/silverstripe-pixlr)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.4@104987 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/ImageEditor/Environment.js
+++
@@ -1,9 +0,0 @@
-/**
- * @author Mateusz
- */
-ImageEditor.Environment = {
-	initialize: function (imageFile) {
-		ImageEditor.imageBox = new ImageEditor.ImageBox.initialize();
-		ImageEditor.imageToResize = new ImageEditor.ImageToResize.initialize(imageFile);		
-	}		
-}
