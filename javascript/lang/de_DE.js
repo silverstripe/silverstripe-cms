1aaa4b069bc9fd1bcaa7af4b02339082e63e9040
Ingo Schommer <ingo@silverstripe.com>
2010-09-04 23:12:56 +0000

    MINOR German translations for cms javascript (#5921, thanks bartlomiej)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.4@110435 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/lang/de_DE.js
+++ javascript/lang/de_DE.js
@@ -22,6 +22,9 @@ if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
 		'ModelAdmin.SAVED': "Gespeichert",
 		'ModelAdmin.REALLYDELETE': "Wirklich löschen?",
 		'ModelAdmin.DELETED': "Gelöscht",
-		'LeftAndMain.PAGEWASDELETED': "Diese Seite wurde gelöscht."
+		'ModelAdmin.VALIDATIONERROR': "Validationsfehler",
+		'LeftAndMain.PAGEWASDELETED': "Diese Seite wurde gelöscht.",
+		'LeftAndMain.CONFIRMUNSAVED': "Sind Sie sicher, dasß Sie die Seite verlassen möchten?\n\nWARNUNG: Ihre Änderungen werden nicht gespeichert.\n\nDrücken Sie \"OK\" um fortzufahren, oder \"Abbrechen\" um auf dieser Seite zu bleiben.",
+		'WidgetAreaEditor.TOOMANY': 'Sie haben die maximale Anzahl an Widgets in diesem Bereich erreicht.'
 	});
 }
