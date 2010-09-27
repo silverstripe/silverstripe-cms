2ec69aa597c7b48b694aa047a4a9cbc1832ab015
Saophalkun Ponlu <phalkunz@silverstripe.com>
2010-09-27 04:32:37 +0000

    MINOR: Add translation correction for Czech and add Slovakian translation in cms and sapphire (js). Thanks to @Pike
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.4@111262 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/lang/cs_CZ.js
+++ javascript/lang/cs_CZ.js
@@ -2,12 +2,12 @@ if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
 	if(typeof(console) != 'undefined') console.error('Class ss.i18n not defined');
 } else {
 	ss.i18n.addDictionary('cs_CZ', {
-		'CMSMAIN.WARNINGSAVEPAGESBEFOREADDING' : "Musíte stránku uložit před přidáním další podstránky",
+		'CMSMAIN.WARNINGSAVEPAGESBEFOREADDING' : "Před přidáním další podstránky, musíte stránku uložit",
 		'CMSMAIN.CANTADDCHILDREN' : "Nemůžete přidat potomky do vybraného uzlu",
 		'CMSMAIN.ERRORADDINGPAGE' : 'Chyba při přidání stránky',
-		'CMSMAIN.FILTEREDTREE' : 'Filtrovaná struktúra k zobrazení pouze zmeněných stránek',
-		'CMSMAIN.ERRORFILTERPAGES' : 'Nemožné filtrovat struktúru k zobrazení pouze zmeněných stránek<br />%s',
-		'CMSMAIN.ERRORUNFILTER' : 'Nemožné filtrovat struktúru webu<br />%s',
+		'CMSMAIN.FILTEREDTREE' : 'Filtrovaná struktura k zobrazení pouze zmeněných stránek',
+		'CMSMAIN.ERRORFILTERPAGES' : 'Není možno filtrovat strukturu k zobrazení pouze zmeněných stránek<br />%s',
+		'CMSMAIN.ERRORUNFILTER' : 'Není možno filtrovat strukturu webu<br />%s',
 		'CMSMAIN.ERRORUNFILTER' : 'Nefiltrovaná struktura',
 		'CMSMAIN.PUBLISHINGPAGES' : 'Zveřejňování stránek...',
 		'CMSMAIN.SELECTONEPAGE' : "Prosím, vyberte nejméně 1 stránku.",
@@ -17,12 +17,12 @@ if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
 		'CMSMAIN.ERRORDELETINGPAGES': 'Chyba při mazání stránek',
 		'CMSMAIN.PUBLISHING' : 'Zveřejňování...',
 		'CMSMAIN.RESTORING': 'Obnovování...',
-		'CMSMAIN.ERRORREVERTING': 'Chybar převádění na živý obsah',
+		'CMSMAIN.ERRORREVERTING': 'Chyba převádění na živý obsah',
 		'CMSMAIN.SAVING' : 'ukládání...',
 		'ModelAdmin.SAVED': "Uloženo",
 		'ModelAdmin.REALLYDELETE': "Skutečně chcete smazat?",
 		'ModelAdmin.DELETED': "Smazáno",
-		'LeftAndMain.PAGEWASDELETED': "Tato stránka byla smazána. K editaci stránky, ji vyberte vlevo.",
+		'LeftAndMain.PAGEWASDELETED': "Tato stránka byla smazána. Pro editaci stránky, vyberte ji vlevo.",
 		'LeftAndMain.CONFIRMUNSAVED': "Určitě chcete opustit navigaci z této stránky?\n\nUPOZORNĚNÍ: Vaše změny nebyly uloženy.\n\nStlačte OK pro pokračovat, nebo Cancel, zůstanete na této stránce."
 	});
 }
---
+++ javascript/lang/sk_SK.js
@@ -0,0 +1,28 @@
+if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
+	if(typeof(console) != 'undefined') console.error('Class ss.i18n not defined');
+} else {
+	ss.i18n.addDictionary('sk_SK', {
+		'CMSMAIN.WARNINGSAVEPAGESBEFOREADDING' : "Pred pridaním ďalšej podstránky, musíte stránku uložiť",
+		'CMSMAIN.CANTADDCHILDREN' : "Nemôžete pridať potomkov do vybratého uzla",
+		'CMSMAIN.ERRORADDINGPAGE' : 'Chyba pri pridaní stránky',
+		'CMSMAIN.FILTEREDTREE' : 'Filtrovaná štruktúra k zobrazeniu iba zmenených stránok',
+		'CMSMAIN.ERRORFILTERPAGES' : 'Nie je možné filtrovať štruktúru k zobrazeniu iba zmenených stránok<br />%s',
+		'CMSMAIN.ERRORUNFILTER' : 'Nie je možné filtrovať štruktúru webu<br />%s',
+		'CMSMAIN.ERRORUNFILTER' : 'Nefiltrovaná štruktúra',
+		'CMSMAIN.PUBLISHINGPAGES' : 'Zverejňovanie stránok...',
+		'CMSMAIN.SELECTONEPAGE' : "Prosím, vyberte najmenej 1 stránku.",
+		'CMSMAIN.ERRORPUBLISHING' : 'Chyba pri zverejňovaní stránok',
+		'CMSMAIN.REALLYDELETEPAGES' : "Skutočne chcete zmazať %s označené stránky?",
+		'CMSMAIN.DELETINGPAGES' : 'Mazanie stránok...',
+		'CMSMAIN.ERRORDELETINGPAGES': 'Chyba pri mazaní stránok',
+		'CMSMAIN.PUBLISHING' : 'Zverejňovanie...',
+		'CMSMAIN.RESTORING': 'Obnovovanie...',
+		'CMSMAIN.ERRORREVERTING': 'Chyba prevádzania na živý obsah',
+		'CMSMAIN.SAVING' : 'ukladanie...',
+		'ModelAdmin.SAVED': "Uložené",
+		'ModelAdmin.REALLYDELETE': "Skutočně chcete zmazať?",
+		'ModelAdmin.DELETED': "Zmazané",
+		'LeftAndMain.PAGEWASDELETED': "Táto stránka bola zmazaná. Pre editáciu stránky, vyberte ju vľavo.",
+		'LeftAndMain.CONFIRMUNSAVED': "Určite chcete opustiť navigáciu z tejto stránky?\n\nUPOZORNENIE: Vaše zmeny neboli uložené.\n\nStlačte OK pre pokračovať, alebo Cancel, ostanete na teto stránke."
+	});
+}
