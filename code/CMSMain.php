75964b85d7790ff1a4da516af288ffe2fe0d12b5
Sam Minnee <sam@silverstripe.com>
2010-10-04 06:16:50 +0000

    MINOR: add more in-line documentation and fix the typo: should => should not (from r103520)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@111668 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/CMSMain.php
+++ code/CMSMain.php
@@ -291,9 +291,11 @@ JS;
 			}
 
 			// Don't open a page from a different locale
-			/** The record's Locale is saved in database in 2.4, and not related with Session, we should check their locale matches the Translatable::get_current_locale,
-			 * 	as long as we all the HTTPRequest is init with right locale.
-			 *	This bit breaks the all FileIFrameField functions if the field is used in CMS and its relevent ajax calles, like loading the tree dropdown for TreeSelectorField. 
+			/** The record's Locale is saved in database in 2.4, and not related with Session,
+			 *  we should not check their locale matches the Translatable::get_current_locale,
+			 * 	here as long as we all the HTTPRequest is init with right locale.
+			 *	This bit breaks the all FileIFrameField functions if the field is used in CMS
+			 *  and its relevent ajax calles, like loading the tree dropdown for TreeSelectorField. 
 			 */
 			/* if($record && Object::has_extension('SiteTree', 'Translatable') && $record->Locale && $record->Locale != Translatable::get_current_locale()) {
 				$record = null;
