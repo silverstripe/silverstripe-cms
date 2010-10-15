66f2fad70a2744915fc82c72775bbbb2cce341ec
Ingo Schommer <ingo@silverstripe.com>
2010-10-15 03:31:30 +0000

    MINOR Made CMSMainTest more resilient against database ID changes (Postgres doesn't have auto-increment resets across tests at the moment) (from r106248)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@112578 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- tests/CMSMainTest.php
+++ tests/CMSMainTest.php
@@ -31,6 +31,8 @@ class CMSMainTest extends FunctionalTest {
 	 * @todo Test the results of a publication better
 	 */
 	function testPublish() {
+		$page1 = $this->objFromFixture('Page', "page1");
+		$page2 = $this->objFromFixture('Page', "page2");
 		$this->session()->inst_set('loggedInAs', $this->idFromFixture('Member', 'admin'));
 		
 		$response = Director::test("admin/cms/publishall", array('confirm' => 1), $this->session());
@@ -42,7 +44,7 @@ class CMSMainTest extends FunctionalTest {
 	
 		// Some modules (e.g., cmsworkflow) will remove this action
 		if(isset(CMSBatchActionHandler::$batch_actions['publish'])) {
-			$response = Director::test("admin/cms/batchactions/publish", array('csvIDs' => '1,2', 'ajax' => 1), $this->session());
+			$response = Director::test("admin/cms/batchactions/publish", array('csvIDs' => implode(',', array($page1->ID, $page2->ID)), 'ajax' => 1), $this->session());
 	
 			$responseData = Convert::json2array($response->getBody());
 			$this->assertTrue(property_exists($responseData['modified'], '1'));
