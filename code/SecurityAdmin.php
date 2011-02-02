silverstripe/silverstripe-cms

commit 97a0abb2cc99d360696768ca9279d421570bc046

MINOR Removed unnecessary "show" icons in "Roles" and "Member" ComplexTableFields
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.4@101752 467b73ca-7a2a-4603-9d3b-597d59a354a9

code/SecurityAdmin.php
@@ -132,7 +132,7 @@ class SecurityAdmin extends LeftAndMain implements PermissionProvider {
 			"Members"
 		);
 		// unset 'inlineadd' permission, we don't want inline addition
-		$memberList->setPermissions(array('show', 'edit', 'delete', 'add'));
+		$memberList->setPermissions(array('edit', 'delete', 'add'));
 		$memberList->setRelationAutoSetting(false);
 		
 		$fields = new FieldSet(
@@ -170,6 +170,7 @@ class SecurityAdmin extends LeftAndMain implements PermissionProvider {
 				'Roles',
 				'PermissionRole'
 			);
+			$rolesCTF->setPermissions(array('edit', 'delete'));
 
 			$rolesTab = $fields->findOrMakeTab('Root.Roles', _t('SecurityAdmin.TABROLES', 'Roles'));
 			$rolesTab->push(new LiteralField(

commit 1d259855c348114cf396af20b86c7d26000d26e5

MINOR Fixed regression from r101752 (adding permission roles button was missing)
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.4@102010 467b73ca-7a2a-4603-9d3b-597d59a354a9

code/SecurityAdmin.php
@@ -170,7 +170,7 @@ class SecurityAdmin extends LeftAndMain implements PermissionProvider {
 				'Roles',
 				'PermissionRole'
 			);
-			$rolesCTF->setPermissions(array('edit', 'delete'));
+			$rolesCTF->setPermissions(array('add', 'edit', 'delete'));
 
 			$rolesTab = $fields->findOrMakeTab('Root.Roles', _t('SecurityAdmin.TABROLES', 'Roles'));
 			$rolesTab->push(new LiteralField(