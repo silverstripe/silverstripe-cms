silverstripe/silverstripe-cms

commit ea20f1101f05763ca426b30372622285fb9bedef

BUGFIX: getting rid of problem with disappearing buttons on the initial page load (not ajax). The initialization was called twice on the CMSForm, and on the second call the ajaxActionsAtTop was removing the buttons - now the buttons are removed only when there is something to be added.
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.4@96551 467b73ca-7a2a-4603-9d3b-597d59a354a9

javascript/LeftAndMain.js
@@ -255,13 +255,13 @@ function unlockStatusMessage() {
  */
 function ajaxActionsAtTop(formName, actionHolderName, tabName) {
 	var actions = document.getElementsBySelector('#' + formName + ' .Actions')[0];
-	var holder;
-
-	if((holder = $(actionHolderName)) && holder != actions) {
-		holder.parentNode.removeChild(holder);
-	}
+	var holder = $(actionHolderName);
 
 	if(actions) {
+		if ( holder ) {
+			holder.parentNode.removeChild(holder);
+		}
+		
 		actions.id = actionHolderName;
 		actions.className = 'ajaxActions';
 

commit 411d0de4a23a6683a2ca3f2e7bc671fee373c34e

BUGFIX: Remove buttons from display if you load a CMS page that should have no buttons - reverts bug caused by r96551 and fixes the issue it was trying to solve.
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.4@99553 467b73ca-7a2a-4603-9d3b-597d59a354a9

javascript/LeftAndMain.js
@@ -266,10 +266,10 @@ function ajaxActionsAtTop(formName, actionHolderName, tabName) {
 	var actions = document.getElementsBySelector('#' + formName + ' .Actions')[0];
 	var holder = $(actionHolderName);
 
+	if ( holder && holder != actions ) {
+		holder.parentNode.removeChild(holder);
+	}
 	if(actions) {
-		if ( holder ) {
-			holder.parentNode.removeChild(holder);
-		}
 		
 		actions.id = actionHolderName;
 		actions.className = 'ajaxActions';

javascript/ReportAdmin_right.js
@@ -1,6 +1,5 @@
-
 Behaviour.register({
-	'#Form_EditForm' : {
+	'body.ReportAdmin #Form_EditForm' : {
 		initialise : function() {
       		this.openTab = null;
 			this.prepareForm();