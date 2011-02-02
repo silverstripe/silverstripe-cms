silverstripe/silverstripe-cms

commit fa04f03b0f5cc138f619f0adc5627618e4c80d7c

BUGFIX Setting  ID explicitly in MemberTableField-> to ensure getCsvQuery() correctly filters (the custom group filter was only implemented in sourceItems() before)
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/trunk@99684 467b73ca-7a2a-4603-9d3b-597d59a354a9

code/MemberTableField.php
@@ -101,6 +101,10 @@ class MemberTableField extends ComplexTableField {
 			}
 			$this->sourceFilter[] = '(' . implode(' OR ', $searchFilters) . ')';
 		}
+		
+		if($this->group) {
+			$this->sourceFilter[] = sprintf('"Group_Members"."GroupID" = %d', $this->group->ID);
+		}
 
 		$this->sourceJoin = " INNER JOIN \"Group_Members\" ON \"MemberID\"=\"Member\".\"ID\"";
 		$this->setFieldListCsv($csvFieldList);

commit f088acf5e39019c8ceb3f43a60d5b853fd97ce50

BUGFIX Fixed MemberTableField limiting of , wasnt taking children groups into account (regression from r99684) (from r99706)
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/cms/branches/2.4@99713 467b73ca-7a2a-4603-9d3b-597d59a354a9

code/MemberTableField.php
@@ -103,7 +103,10 @@ class MemberTableField extends ComplexTableField {
 		}
 		
 		if($this->group) {
-			$this->sourceFilter[] = sprintf('"Group_Members"."GroupID" = %d', $this->group->ID);
+			$this->sourceFilter[] = sprintf(
+				'"Group_Members"."GroupID" IN (%s)', 
+				implode(',', array_merge($this->group->AllChildren()->column('ID'), array($this->group->ID)))
+			);
 		}
 
 		$this->sourceJoin = " INNER JOIN \"Group_Members\" ON \"MemberID\"=\"Member\".\"ID\"";