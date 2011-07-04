c4a99dfc095d85ec639d7e9523a2cf683c6cbcbb
Ingo Schommer <ingo@silverstripe.com>
2011-07-04 16:09:07 +0200

    MINOR Redirecting page links in CMSPagesController to CMSPageEditController (admin/pages/show/99 to admin/page/show/99)

--- javascript/CMSPagesController.Tree.js
+++
@@ -1,7 +0,0 @@
-(function($) {
-	$('.cms-tree').bind('select_node.jstree', function(e, data) {
-		var node = data.rslt.obj, url = $(node).find('a:first').attr('href');
-		if(url && url != '#') document.location.href = url;
-		return false;
-	});
-}(jQuery));
