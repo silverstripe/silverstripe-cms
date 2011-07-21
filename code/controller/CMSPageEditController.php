07094f5b10460854eb9292be9e4a0d2016455f8f
Ingo Schommer <ingo@silverstripe.com>
2011-07-21 20:01:33 +0200

    MINOR Removed temporary preview links in CMSPageEditController

--- code/controller/CMSPageEditController.php
+++ code/controller/CMSPageEditController.php
@@ -4,18 +4,5 @@ class CMSPageEditController extends CMSMain {
 	static $url_segment = 'page/edit';
 	static $url_rule = '/$Action/$ID/$OtherID';
 	static $url_priority = 41;
-	
-	function getEditForm($id = null, $fields = null) {
-		$record = $this->getRecord($id ? $id : $this->currentPageID());
-		$form = parent::getEditForm($record, ($record) ? $record->getCMSFields() : null);
 		
-		// TODO Replace with preview button
-		$form->Fields()->addFieldToTab(
-			'Root.Main', 
-			new LiteralField('SwitchView', sprintf('<div class="cms-switch-view field"><label>Preview:</label><div class="middleColumn">%s</div></div>', $this->SwitchView()))
-		);
-		
-		return $form;
-	}
-	
 }
