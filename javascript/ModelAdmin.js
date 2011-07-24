7f350b442694ab3b7398c9b0c98bcc6db6de3c38
Simon Erkelens <simon@squal.nl>
2011-07-24 15:19:25 -0700

    This addition makes the ModelAdmin do a default (empty) search when clicked/tab-select/dropdown. Lot of customers complained about it.

--- javascript/ModelAdmin.js
+++ javascript/ModelAdmin.js
@@ -17,6 +17,32 @@ $(document).ready(function() {
 			$('input', this).removeClass('loading');
 			statusMessage(ss.i18n._t('ModelAdmin.ERROR', 'Error'), 'bad');
 	});
+
+        /***********************************************************************
+         * perform default search on DO select
+         */
+        if ($('#SearchForm_holder .tabstrip').length > 0) {
+            /**
+             * tabstrip may not be initialized when this is executed
+             */
+            // the initial search
+            var activeTab = ($('#SearchForm_holder .tabstrip li:first a:first').attr('href')).replace(/^.*#/, '');
+            $('#Form_Search' + activeTab).submit();
+
+            // the onchange search
+            $('#SearchForm_holder .tabstrip li a').live('click', function() {
+                var activeTab = ($(this).attr('href')).replace(/^.*#/, '');
+                $('#Form_Search' + activeTab).submit();
+            });
+        } else if ($('#ModelClassSelector').length > 0) {
+            // the initial search
+            $('#Form_Search' + $('#ModelClassSelector select').val()).submit();
+
+            // the onchange search
+            $('#ModelClassSelector select').change(function(){
+                $('#Form_Search' + $('#ModelClassSelector select').val()).submit();
+            });
+        }
 	
 	// We're avoiding livequery initializers for now,
 	// to be replaced by jQuery.entwine.
