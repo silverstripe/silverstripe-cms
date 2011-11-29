527e6ed4e1c1d75ebca088a8a2da23cdd44cf48f
Ingo Schommer <ingo@silverstripe.com>
2011-11-29 09:45:34 +0100

    BUGFIX Consistently using multibyte-safe htmlentities()

--- code/CMSMain.php
+++ code/CMSMain.php
@@ -1048,7 +1048,7 @@ JS;
 		}
 
 		return array(
-			"Message" => htmlentities($_REQUEST['Message']),
+			"Message" => htmlentities($_REQUEST['Message'], ENT_COMPAT, 'UTF-8'),
 			"Buttons" => $buttons,
 			"Modal" => $_REQUEST['Modal'] ? true : false,
 		);
