213cfb031ed11e9ae0be30deb11ddabd6de911b0
Ingo Schommer <ingo@silverstripe.com>
2012-01-09 11:38:10 +0100

    MINOR Added routing for AssetAdmin to cms/_config.php (required for GridField?)

--- _config.php
+++ _config.php
@@ -9,6 +9,7 @@ Director::addRules(50, array(
 	'' => 'RootURLController',
 	'admin/bulkload//$Action/$ID/$OtherID' => 'BulkLoaderAdmin',
 	'admin/cms//$Action/$ID/$OtherID' => 'CMSMain', 
+	'admin/asset//$Action/$ID/$OtherID' => 'AssetAdmin', 
 	'dev/buildcache/$Action' => 'RebuildStaticCacheTask',
 ));
 
