7de585103f35dbcfbf9986ef78b0f6b50d213d33
Stig Lindqvist <stig@silverstripe.com>
2012-01-10 14:02:19 +1300

    BUGFIX: Can't upload files to root folder

--- code/controllers/AssetAdmin.php
+++ code/controllers/AssetAdmin.php
@@ -202,8 +202,11 @@ JS
 		}
 		$processedData = array_reverse($processedData);
 				
-		if($data['FolderID'] && $data['FolderID'] != '') $folder = DataObject::get_by_id("Folder", $data['FolderID']);
-		else $folder = singleton('Folder');
+		if(isset($data['FolderID']) && is_numeric($data['FolderID'])) {
+			$folder = DataObject::get_by_id("Folder", $data['FolderID']);
+		} else {
+			$folder = singleton('Folder');
+		}
 
 		foreach($processedFiles as $filePostId => $tmpFile) {
 			if($tmpFile['error'] == UPLOAD_ERR_NO_TMP_DIR) {
