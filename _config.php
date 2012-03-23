2f8148d0c04c25d0825cb5653825045c7c317a80
Simon Welsh <simon@simon.geek.nz>
2012-03-24 11:57:49 +1300

    MINOR Move definition of CMS_DIR and CMS_PATH into cms/_config.php

--- _config.php
+++ _config.php
@@ -1,6 +1,13 @@
 <?php
 
 /**
+ * - CMS_DIR: Path relative to webroot, e.g. "cms"
+ * - CMS_PATH: Absolute filepath, e.g. "/var/www/my-webroot/cms"
+ */
+define('CMS_DIR', 'cms');
+define('CMS_PATH', BASE_PATH . '/' . CMS_DIR);
+
+/**
  * Extended URL rules for the CMS module
  * 
  * @package cms
