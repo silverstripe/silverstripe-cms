3abd264c30568792d8a62301bdfb8062298d9be5
Ingo Schommer <ingo@silverstripe.com>
2012-04-03 01:15:10 +0200

    MINOR Setting new default theme in standard mysite/_config.php

--- mysite/_config.php
+++ mysite/_config.php
@@ -12,7 +12,7 @@ MySQLDatabase::set_connection_charset('utf8');
 
 // Set the current theme. More themes can be downloaded from
 // http://www.silverstripe.org/themes/
-SSViewer::set_theme('blackcandy');
+SSViewer::set_theme('simple');
 
 // Enable nested URLs for this site (e.g. page/sub-page/)
 if(class_exists('SiteTree')) SiteTree::enable_nested_urls();
