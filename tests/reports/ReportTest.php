ee220bbccec4c40258c74288ba30c383b5ed6868
Julian Seidenberg <julian@silverstripe.com>
2012-04-03 18:06:35 +1200

    API-CHANGE: SSF-168 initial rearranging the SS_Report class for use in SS3. Deprecating unnecessary methods, moving code around, rewriting get_reports method and writing unit tests

--- tests/reports/ReportTest.php
+++ tests/reports/ReportTest.php
@@ -1,18 +1,34 @@
 <?php
 
 class ReportTest extends SapphireTest {
-	
-	function testCanSortBy() {
-		$report = new SSReportTest_FakeTest();
-		$this->assertTrue($report->sourceQuery(array())->canSortBy('Title ASC'));
-		$this->assertTrue($report->sourceQuery(array())->canSortBy('Title DESC'));
-		$this->assertTrue($report->sourceQuery(array())->canSortBy('Title'));
-	}
 
 	function testGetReports() {
 		$reports = SS_Report::get_reports();
 		$this->assertNotNull($reports, "Reports returned");
-		Debug::Show($reports);
+		$previousSort = 0;
+		foreach($reports as $report) {
+			$this->assertGreaterThanOrEqual($previousSort, $report->sort, "Reports are in correct sort order");
+			$previousSort = $report->sort;
+		}
+	}
+
+	function testExcludeReport() {
+		$reports = SS_Report::get_reports();
+		$reportNames = array();
+		foreach($reports as $report) {
+			$reportNames[] = $report->class;
+		}
+		$this->assertContains('ReportTest_FakeTest',$reportNames,'ReportTest_FakeTest is in reports list');
+
+		//excluse one report
+		SS_Report::excludeReport('ReportTest_FakeTest');
+
+		$reports = SS_Report::get_reports();
+		$reportNames = array();
+		foreach($reports as $report) {
+			$reportNames[] = $report->class;
+		}
+		$this->assertNotContains('ReportTest_FakeTest',$reportNames,'ReportTest_FakeTest is NOT in reports list');
 	}
 }
 
@@ -39,12 +55,12 @@ class ReportTest_FakeTest extends SS_Report implements TestOnly {
 
 class ReportTest_FakeTest2 extends SS_Report implements TestOnly {
 	function title() {
-		return 'Report title';
+		return 'Report title 2';
 	}
 	function columns() {
 		return array(
 			"Title" => array(
-				"title" => "Page Title"
+				"title" => "Page Title 2"
 			)
 		);
 	}
