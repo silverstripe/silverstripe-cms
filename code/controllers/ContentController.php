3fbf57245c706ead38869ddb000e49cce1faea7b
Sam Minnee <sam@silverstripe.com>
2012-06-22 14:05:27 +1200

    REMOVE: Remove action handler for widgets. (Trac #7174)
    The widget functionality has been moved into a module, and this URL handler was the last of the code still in the core app related to widgets.  As well as this commit, there is a related change to the silverstripe-widgets module to add this feature there: https://github.com/silverstripe/silverstripe-widgets/commit/025ee424c4aa27f23513bad36cdaf136b3b9cb64

--- code/controllers/ContentController.php
+++ code/controllers/ContentController.php
@@ -22,10 +22,6 @@ class ContentController extends Controller {
 
 	protected $dataRecord;
 
-	static $url_handlers = array(
-		'widget/$ID!' => 'handleWidget'
-	);
-	
 	public static $allowed_actions = array(
 		'successfullyinstalled',
 		'deleteinstallfiles' // secured through custom code
@@ -214,52 +210,6 @@ class ContentController extends Controller {
 		} else {
 			throw new SS_HTTPResponse_Exception($response);
 		}
-	}
-	
-	/**
-	 * Handles widgets attached to a page through one or more {@link WidgetArea} elements.
-	 * Iterated through each $has_one relation with a {@link WidgetArea}
-	 * and looks for connected widgets by their database identifier.
-	 * Assumes URLs in the following format: <URLSegment>/widget/<Widget-ID>.
-	 * 
-	 * @return RequestHandler
-	 */
-	function handleWidget() {
-		$SQL_id = $this->request->param('ID');
-		if(!$SQL_id) return false;
-		
-		// find WidgetArea relations
-		$widgetAreaRelations = array();
-		$hasOnes = $this->dataRecord->has_one();
-		if(!$hasOnes) return false;
-		foreach($hasOnes as $hasOneName => $hasOneClass) {
-			if($hasOneClass == 'WidgetArea' || is_subclass_of($hasOneClass, 'WidgetArea')) {
-				$widgetAreaRelations[] = $hasOneName;
-			}
-		}
-
-		// find widget
-		$widget = null;
-		foreach($widgetAreaRelations as $widgetAreaRelation) {
-			if($widget) break;
-			$widget = $this->dataRecord->$widgetAreaRelation()->Widgets(
-				sprintf('"Widget"."ID" = %d', $SQL_id)
-			)->First();
-		}
-		if(!$widget) user_error('No widget found', E_USER_ERROR);
-		
-		// find controller
-		$controllerClass = '';
-		foreach(array_reverse(ClassInfo::ancestry($widget->class)) as $widgetClass) {
-			$controllerClass = "{$widgetClass}_Controller";
-			if(class_exists($controllerClass)) break;
-		}
-		if(!$controllerClass) user_error(
-			sprintf('No controller available for %s', $widget->class),
-			E_USER_ERROR
-		);
-
-		return new $controllerClass($widget);
 	}
 
 	/**
