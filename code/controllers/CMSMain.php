silverstripe/silverstripe-cms

commit e7ecdf4b350b9b846ca735fe26f4b377bcccbcbf

BUGFIX: Fixed page tree view

code/controllers/CMSMain.php
@@ -701,8 +701,8 @@ class CMSMain extends LeftAndMain implements CurrentPageIdentifier, PermissionPr
 
 		$controller = $this;
 		$columns->setFieldFormatting(array(
-			'listChildrenLink' => function(&$item) use($controller) {
-				$num = $item->numChildren();
+			'listChildrenLink' => function($value, &$item) use($controller) {
+				$num = $item ? $item->numChildren() : null;
 				if($num) {
 					return sprintf(
 						'<a class="cms-panel-link list-children-link" data-pjax-target="ListViewForm,Breadcrumbs" href="%s?ParentID=%d&view=list">%s</a>',
@@ -712,8 +712,9 @@ class CMSMain extends LeftAndMain implements CurrentPageIdentifier, PermissionPr
 					);
 				}
 			},
-			'getTreeTitle' => '<a class=\"cms-panel-link\" href=\"' . 
-				singleton('CMSPageEditController')->Link('show') . '/$ID\">$value</a>'
+			'getTreeTitle' => function($value, &$item) use($controller) {
+				return '<a class="cms-panel-link" href="' . $controller->Link('show') . '/' . $item->ID . '">' . $item->TreeTitle . '</a>';
+			}
 		));
 		
 		$listview = new Form(

commit d77c43d6c703b9646c6b46855d2425e5af8665f5

BUG CMS pages list view link targets (fixes #7589)
Previously broken in e7ecdf4b350

code/controllers/CMSMain.php
@@ -715,7 +715,7 @@ class CMSMain extends LeftAndMain implements CurrentPageIdentifier, PermissionPr
 				}
 			},
 			'getTreeTitle' => function($value, &$item) use($controller) {
-				return '<a class="cms-panel-link" href="' . $controller->Link('show') . '/' . $item->ID . '">' . $item->TreeTitle . '</a>';
+				return '<a class="cms-panel-link" href="' . singleton('CMSPageEditController')->Link('show') . '/' . $item->ID . '">' . $item->TreeTitle . '</a>';
 			}
 		));
 		