76be14b1fccc05b5dcca8c805e0354a42e75135d
Damian Mooyman <damian.mooyman@gmail.com>
2012-08-24 08:28:18 +1200

    FIXED: Indentation

--- code/model/SiteConfig.php
+++ code/model/SiteConfig.php
@@ -52,20 +52,20 @@ class SiteConfig extends DataObject implements PermissionProvider {
 		$fields = new FieldList(
 			new TabSet("Root",
 				$tabMain = new Tab('Main',
-						$titleField = new TextField("Title", _t('SiteConfig.SITETITLE', "Site title")),
-						$taglineField = new TextField("Tagline", _t('SiteConfig.SITETAGLINE', "Site Tagline/Slogan")),
-						$themeDropdownField = new DropdownField("Theme", _t('SiteConfig.THEME', 'Theme'), $this->getAvailableThemes())
+					$titleField = new TextField("Title", _t('SiteConfig.SITETITLE', "Site title")),
+					$taglineField = new TextField("Tagline", _t('SiteConfig.SITETAGLINE', "Site Tagline/Slogan")),
+					$themeDropdownField = new DropdownField("Theme", _t('SiteConfig.THEME', 'Theme'), $this->getAvailableThemes())
 				),
 				$tabAccess = new Tab('Access',
-						$viewersOptionsField = new OptionsetField("CanViewType", _t('SiteConfig.VIEWHEADER', "Who can view pages on this site?")),
-						$viewerGroupsField = ListboxField::create("ViewerGroups", _t('SiteTree.VIEWERGROUPS', "Viewer Groups"))
-								->setMultiple(true)->setSource($groupsMap),
-						$editorsOptionsField = new OptionsetField("CanEditType", _t('SiteConfig.EDITHEADER', "Who can edit pages on this site?")),
-						$editorGroupsField = ListboxField::create("EditorGroups", _t('SiteTree.EDITORGROUPS', "Editor Groups"))
-								->setMultiple(true)->setSource($groupsMap),
-						$topLevelCreatorsOptionsField = new OptionsetField("CanCreateTopLevelType", _t('SiteConfig.TOPLEVELCREATE', "Who can create pages in the root of the site?")),
-						$topLevelCreatorsGroupsField = ListboxField::create("CreateTopLevelGroups", _t('SiteTree.TOPLEVELCREATORGROUPS', "Top level creators"))
-								->setMultiple(true)->setSource($groupsMap)
+					$viewersOptionsField = new OptionsetField("CanViewType", _t('SiteConfig.VIEWHEADER', "Who can view pages on this site?")),
+					$viewerGroupsField = ListboxField::create("ViewerGroups", _t('SiteTree.VIEWERGROUPS', "Viewer Groups"))
+						->setMultiple(true)->setSource($groupsMap),
+					$editorsOptionsField = new OptionsetField("CanEditType", _t('SiteConfig.EDITHEADER', "Who can edit pages on this site?")),
+					$editorGroupsField = ListboxField::create("EditorGroups", _t('SiteTree.EDITORGROUPS', "Editor Groups"))
+						->setMultiple(true)->setSource($groupsMap),
+					$topLevelCreatorsOptionsField = new OptionsetField("CanCreateTopLevelType", _t('SiteConfig.TOPLEVELCREATE', "Who can create pages in the root of the site?")),
+					$topLevelCreatorsGroupsField = ListboxField::create("CreateTopLevelGroups", _t('SiteTree.TOPLEVELCREATORGROUPS', "Top level creators"))
+						->setMultiple(true)->setSource($groupsMap)
 				)
 			),
 			new HiddenField('ID')
