f454f481f25e25406e7b0236d89f19a65a4ea58c
Ingo Schommer <ingo@silverstripe.com>
2012-08-28 00:28:58 +0200

    BUG Session namespace sharing for CMS controllers
    Ideally we could do this without session, but pragmatically
    we still need it, because of the inflexible routing system,
    and because of performance considerations.
    
    Example: The tree is lazy loaded via a generic  URL (admin/pages/treeview).
    While we could add ?ID=<currentpage> to make the view (more or less) stateless,
    it would trigger a full tree reload on every tree navigation action.
    Instead, we assume that all "reachable" nodes are already cached,
    and simply mark a different one as current. For this to work, we need
    shared session state between CMS controllers.
    
    See http://open.silverstripe.org/ticket/7815 for detail.

--- code/controllers/CMSPageSettingsController.php
+++ code/controllers/CMSPageSettingsController.php
@@ -9,6 +9,7 @@ class CMSPageSettingsController extends CMSMain {
 	static $url_rule = '/$Action/$ID/$OtherID';
 	static $url_priority = 42;
 	static $required_permission_codes = 'CMS_ACCESS_CMSMain';
+	static $session_namespace = 'CMSMain';
 		
 	function getEditForm($id = null, $fields = null) {
 		$record = $this->getRecord($id ? $id : $this->currentPageID());
