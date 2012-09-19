48719e6d95a0a5ee25937a93817aaab41f0982a0
Will Rossiter <will.rossiter@gmail.com>
2012-09-19 21:05:40 +1200

    NEW: Implements StaticExporter_Task for generating an export through sake.

--- code/controllers/StaticExporter.php
+++ code/controllers/StaticExporter.php
@@ -1,19 +1,41 @@
 <?php
 /**
- * This class lets you export a static copy of your site.
- * It creates a huge number of folders each containing an index.html file.
- * This preserves the URL naming format.
- * 
- * Requirements: Unix Filesystem supporting symlinking. Doesn't work on Windows.
+ * This class lets you export a static copy of your site either as an tar 
+ * archive through the web browser or through the command line.
+ *
+ * The exporter will preserve the URL naming format of your pages by 
+ * creating a number of subfolders folders each containing an index.html 
+ * file.
+ *
+ * <b>Requirements:</b>
+ *	- Unix Filesystem supporting symlinking. 
+ *	- Tar installed (for generating an archive)
+ *	- Doesn't work on Windows.
  * 
  * <b>Usage</b>
  * 
- * The exporter can only be invoked through a URL. Usage on commandline (through [sake](sake)) is not possible at the moment, as we're sending a file to the browser for download.
- * 
- * <pre>http://localhost/StaticExporter/export</pre>
- * 
- * Specify a custom baseurl in case you want to deploy the static HTML pages on a different host:
- * <pre>http://localhost/StaticExporter/export?baseurl=http://example.com</pre>
+ * The exporter can be invoked through a URL or on the commandline (through 
+ * [sake](sake)).
+ *
+ * To download a tar.gz through the browser visit the following URL while
+ * logged in as an administrator:
+ *
+ * <code>
+ * http://localhost/StaticExporter/
+ * </code>
+ *
+ * Or you can generate the export via sake:
+ *
+ * <code>
+ * sake dev/tasks/StaticExporter_Task "baseurl=http://example.com"
+ * </code>
+ *
+ * <b>Options</b>
+ *
+ * Specify a custom baseurl in case you want to deploy the static HTML pages on 
+ * a different host:
+ *
+ * <code>http://localhost/StaticExporter/export?baseurl=http://example.com</pre>
  * 
  * @see StaticPublisher
  * 
@@ -22,12 +44,13 @@
  */
 class StaticExporter extends Controller {
 
-	static $allowed_actions = array(
+	public static $allowed_actions = array(
 		'index', 
 		'export', 
+		'StaticExportForm'
 	);
 
-	function init() {
+	public function init() {
 		parent::init();
 		
 		$canAccess = (Director::isDev() || Director::is_cli() || Permission::check("ADMIN"));
@@ -35,80 +58,165 @@ class StaticExporter extends Controller {
 	}
 		
 	
-	function Link($action = null) {
+	public function Link($action = null) {
 		return "StaticExporter/$action";
 	}
 	
-	function index() {
-		echo "<h1>"._t('StaticExporter.NAME','Static exporter')."</h1>";
-		echo $this->StaticExportForm()->forTemplate();
+	public function index() {
+		return array(
+			'Title' => _t('StaticExporter.NAME','Static exporter'),
+			'Form' => $this->StaticExportForm()->forTemplate()
+		);
 	}
 	
-	function StaticExportForm() {
-		return new Form($this, 'StaticExportForm', new FieldList(
-			// new TextField('folder', _t('StaticExporter.FOLDEREXPORT','Folder to export to')),
+	public function StaticExportForm() {
+		$form = new Form($this, 'StaticExportForm', new FieldList(
 			new TextField('baseurl', _t('StaticExporter.BASEURL','Base URL'))
 		), new FieldList(
-			new FormAction('export', _t('StaticExporter.EXPORTTO','Export to that folder'))
+			new FormAction('export', _t('StaticExporter.EXPORT','Export'))
 		));
+
+		return $form;
 	}
-	
-	function export() {
-		// specify custom baseurl for publishing to other webroot
+
+
+	public function export() {
 		if(isset($_REQUEST['baseurl'])) {
 			$base = $_REQUEST['baseurl'];
 			if(substr($base,-1) != '/') $base .= '/';
-			Director::setBaseURL($base);
 		}
-		
-		// setup temporary folders
-		$tmpBaseFolder = TEMP_FOLDER . '/static-export';
-		$tmpFolder = (project()) ? "$tmpBaseFolder/" . project() : "$tmpBaseFolder/site";
-		if(!file_exists($tmpFolder)) Filesystem::makeFolder($tmpFolder);
-		$baseFolderName = basename($tmpFolder);
-
-		// symlink /assets
-		$f1 = ASSETS_PATH;
-		$f2 = Director::baseFolder() . '/' . project();
-		`cd $tmpFolder; ln -s $f1; ln -s $f2`;
-
-		// iterate through all instances of SiteTree
-		$pages = DataObject::get("SiteTree");
-		foreach($pages as $page) {
-			$subfolder   = "$tmpFolder/" . trim($page->RelativeLink(null, true), '/');
-			$contentfile = "$tmpFolder/" . trim($page->RelativeLink(null, true), '/') . '/index.html';
-			
-			// Make the folder				
-			if(!file_exists($subfolder)) {
-				Filesystem::makeFolder($subfolder);
-			}
-			
-			// Run the page
-			Requirements::clear();
-			$link = Director::makeRelative($page->Link());
-			$response = Director::test($link);
-
-			// Write to file
-			if($fh = fopen($contentfile, 'w')) {
-				fwrite($fh, $response->getBody());
-				fclose($fh);
-			}
+		else {
+			$base = Director::baseURL();
 		}
 
-		// copy homepage (URLSegment: "home") to webroot
-		copy("$tmpFolder/home/index.html", "$tmpFolder/index.html");			
+		$folder = TEMP_FOLDER . '/static-export';
+		$project = project();
+
+		$exported = $this->doExport($base, $folder .'/'. $project, false);
+
+		`cd $folder; tar -czhf $project-export.tar.gz $project`;
+
+		$archiveContent = file_get_contents("$folder/$project-export.tar.gz");
 		
-		// archive all generated files
-		`cd $tmpBaseFolder; tar -czhf $baseFolderName.tar.gz $baseFolderName`;
-		$archiveContent = file_get_contents("$tmpBaseFolder/$baseFolderName.tar.gz");
-		
-		// remove temporary files and folder
-		Filesystem::removeFolder($tmpBaseFolder);
 		
 		// return as download to the client
-		$response = SS_HTTPRequest::send_file($archiveContent, "$baseFolderName.tar.gz", 'application/x-tar-gz');
+		$response = SS_HTTPRequest::send_file(
+			$archiveContent, 
+			"$project-export.tar.gz", 
+			'application/x-tar-gz'
+		);
+		
 		echo $response->output();
 	}
-	
+
+	/**
+	 * Exports the website with the given base url. Returns the path where the
+	 * exported version of the website is located.
+	 *
+	 * @param string website base url
+	 * @param string folder to export the site into
+	 * @param bool symlink assets
+	 * @param bool suppress output progress
+	 *
+	 * @return string path to export
+	 */
+	public function doExport($base, $folder, $symlink = true, $quiet = true) {
+		ini_set('max_execution_time', 0);
+		Director::setBaseURL($base);
+
+		if(is_dir($folder)) {
+			Filesystem::removeFolder($folder);
+		}
+
+		Filesystem::makeFolder($folder);
+		
+		// symlink or copy /assets
+		$f1 = ASSETS_PATH;
+		$f2 = Director::baseFolder() . '/' . project();
+
+		if($symlink) {
+			`cd $folder; ln -s $f1; ln -s $f2`;
+		}
+		else {
+			`cp -R $f1 $folder; cp -R $f2 $folder`;
+		}
+
+		// iterate through items we need to export
+		$objs = $this->getObjectsToExport();
+
+		if($objs) {
+			$total = $objs->count();
+			$i = 1;
+
+			foreach($objs as $obj) {
+				$link = $obj->RelativeLink(null, true);
+
+				$subfolder   = "$folder/" . trim($link, '/');
+				$contentfile = "$folder/" . trim($link, '/') . '/index.html';
+				
+				// Make the folder				
+				if(!file_exists($subfolder)) {
+					Filesystem::makeFolder($subfolder);
+				}
+				
+				// Run the page
+				Requirements::clear();
+				$link = Director::makeRelative($obj->Link());
+				$response = Director::test($link);
+
+				// Write to file
+				if($fh = fopen($contentfile, 'w')) {
+					if(!$quiet) printf("-- (%s/%s) Outputting page (%s)%s", $i, $total, $obj->RelativeLink(null, true), PHP_EOL);
+
+					fwrite($fh, $response->getBody());
+					fclose($fh);
+				}
+
+				$i++;
+			}
+		}
+
+		return $folder;
+	}
+
+	/**
+	 * Return a list of publishable instances for the exporter to include. The
+	 * only requirement is that for this list of objects, each one implements
+	 * the RelativeLink() and Link() method.
+	 *
+	 * @return SS_List
+	 */
+	public function getObjectsToExport() {
+		$objs = SiteTree::get();
+		$this->extend('alterObjectsToExport', $objs);
+
+		return $objs;
+	}
 }
 
+/**
+ * @package cms
+ * @subpackage export
+ */
+class StaticExporter_Task extends BuildTask {
+
+	public function run($request) {
+		$export = new StaticExporter();
+
+		$url = $request->getVar('baseurl');
+		$sym = $request->getVar('symlink');
+		$quiet = $request->getVar('quiet');
+		$folder = $request->getVar('path');
+
+		if(!$folder) $folder = TEMP_FOLDER . '/static-export';
+
+		$url = ($url) ? $url : Director::baseURL(); 
+		$symlink = ($sym != "false");
+		$quiet = ($quiet) ? $quiet : false;
+
+		if(!$quiet) printf("Exporting website with %s base URL... %s", $url, PHP_EOL);
+		$path = $export->doExport($url, $folder, $symlink, $quiet);
+
+		if(!$quiet) printf("Completed. Website exported to %s. %s", $path, PHP_EOL);
+	}
+}
