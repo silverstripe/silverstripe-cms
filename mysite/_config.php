744605d21a8e58966611a4c774b489657f8097ac
Ingo Schommer <ingo@silverstripe.com>
2012-11-08 14:03:39 +0100

    Removed .mergesources.yml, not used since the dark SVN days

--- mysite/_config.php
+++ mysite/_config.php
@@ -4,7 +4,7 @@ global $project;
 $project = 'mysite';
 
 global $database;
-$database = '';
+$database = 'SS_ss30';
 
 require_once('conf/ConfigureFromEnv.php');
 
@@ -14,5 +14,8 @@ MySQLDatabase::set_connection_charset('utf8');
 // http://www.silverstripe.org/themes/
 SSViewer::set_theme('simple');
 
+// Set the site locale
+i18n::set_locale('en_US');
+
 // Enable nested URLs for this site (e.g. page/sub-page/)
-if(class_exists('SiteTree')) SiteTree::enable_nested_urls();
+if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
