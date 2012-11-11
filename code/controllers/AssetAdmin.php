d58b23d6fa145bcd36af504240d38cede20de7fd
Kirk Mayo <kirk@silverstripe.com>
2012-11-12 12:51:00 +1300

    BUG AssetAdmin filter array indices (fixes #8014)

--- code/controllers/AssetAdmin.php
+++ code/controllers/AssetAdmin.php
@@ -120,11 +120,13 @@ JS
 
 		// Category filter
 		if(isset($params['AppCategory'])) {
-			$exts = File::$app_categories[$params['AppCategory']];
+			$exts = (isset(File::$app_categories[$params['AppCategory']])) ? File::$app_categories[$params['AppCategory']] : array();
 			$categorySQLs = array();
 			foreach($exts as $ext) $categorySQLs[] = '"File"."Name" LIKE \'%.' . $ext . '\'';
 			// TODO Use DataList->filterAny() once OR connectives are implemented properly
-			$list = $list->where('(' . implode(' OR ', $categorySQLs) . ')');
+			if (count($categorySQLs) > 0) {
+				$list = $list->where('(' . implode(' OR ', $categorySQLs) . ')');
+			}
 		}
 
 		return $list;
