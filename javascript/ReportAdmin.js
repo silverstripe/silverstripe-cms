98e824bcedc56896d23a533c8523dee3115a53ce
Ingo Schommer <ingo@silverstripe.com>
2012-12-04 10:22:29 +0100

    Avoid duplicating ReportAdmin search params (fixes #8046)

--- javascript/ReportAdmin.js
+++ javascript/ReportAdmin.js
@@ -6,7 +6,8 @@
 	$.entwine('ss', function($){
 		$('.ReportAdmin .cms-edit-form').entwine({
 			onsubmit: function(e) {
-				var url = document.location.href, params = this.find(':input[name^=filters]').serializeArray();
+				var url = $.path.parseUrl(document.location.href).hrefNoSearch, 
+					params = this.find(':input[name^=filters]').serializeArray();
 				params = $.grep(params, function(param) {return (param.value);}); // filter out empty
 				if(params) url = $.path.addSearchParams(url, params);
 				$('.cms-container').loadPanel(url);
