b15b98345f96f57aea4bc0a6e539cc206e42a19c
Ingo Schommer <ingo@silverstripe.com>
2013-01-29 17:31:25 +0100

    Using new "Security.login_url" configuration value

--- code/controllers/ContentController.php
+++ code/controllers/ContentController.php
@@ -302,7 +302,12 @@ class ContentController extends Controller {
 				$surname = Convert::raw2xml($member->Surname);
 				$logInMessage = _t('ContentController.LOGGEDINAS', 'Logged in as') ." {$firstname} {$surname} - <a href=\"Security/logout\">". _t('ContentController.LOGOUT', 'Log out'). "</a>";
 			} else {
-				$logInMessage = _t('ContentController.NOTLOGGEDIN', 'Not logged in') ." - <a href=\"Security/login\">". _t('ContentController.LOGIN', 'Login') ."</a>";
+				$logInMessage = sprintf(
+					'%s - <a href="%s">%s</a>' ,
+					_t('ContentController.NOTLOGGEDIN', 'Not logged in') ,
+					Config::inst()->get('Security', 'login_url'),
+					_t('ContentController.LOGIN', 'Login') ."</a>"
+				);
 			}
 			$viewPageIn = _t('ContentController.VIEWPAGEIN', 'View Page in:');
 			
