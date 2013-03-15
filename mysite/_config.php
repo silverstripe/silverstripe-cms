bac9e2fc0060d474b3470a878dd31a2af74c4ee6
Ingo Schommer <ingo@silverstripe.com>
2013-03-15 18:02:21 +0100

    Added mysite/_config/config.yml, removed obsolete PHP config (#8317)

--- mysite/_config.php
+++ mysite/_config.php
@@ -8,12 +8,5 @@ $database = '';
 
 require_once('conf/ConfigureFromEnv.php');
 
-// Set the current theme. More themes can be downloaded from
-// http://www.silverstripe.org/themes/
-SSViewer::set_theme('simple');
-
 // Set the site locale
 i18n::set_locale('en_US');
-
-// Enable nested URLs for this site (e.g. page/sub-page/)
-if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
