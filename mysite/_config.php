f8762ef204cd9174dd8f701ca279907700a03b70
Ingo Schommer <ingo@silverstripe.com>
2013-03-15 18:01:50 +0100

    Removed MySQLDatabase::set_connection_charset() from default config
    Its now part of the core default YAML settings

--- mysite/_config.php
+++ mysite/_config.php
@@ -8,11 +8,12 @@ $database = '';
 
 require_once('conf/ConfigureFromEnv.php');
 
-MySQLDatabase::set_connection_charset('utf8');
-
 // Set the current theme. More themes can be downloaded from
 // http://www.silverstripe.org/themes/
 SSViewer::set_theme('simple');
 
+// Set the site locale
+i18n::set_locale('en_US');
+
 // Enable nested URLs for this site (e.g. page/sub-page/)
-if(class_exists('SiteTree')) SiteTree::enable_nested_urls();
+if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
