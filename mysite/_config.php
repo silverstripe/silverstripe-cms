7ca1093813a733e66986d4a22d13446ab46f09bc
Robert Curry <robert@silverstripe.com>
2013-03-26 16:29:05 +1300

    Add advancedworkflow module

--- mysite/_config.php
+++ mysite/_config.php
@@ -24,3 +24,6 @@ date_default_timezone_set('Pacific/Auckland');
 if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
 
 SiteTree::set_create_default_pages(false);
+
+Object::add_extension('SiteTree', 'WorkflowApplicable');
+Object::add_extension('SiteTree', 'WorkflowEmbargoExpiryExtension');
