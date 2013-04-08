6482eeb13711744477b7bef5d5a370925fc881c2
Simon Welsh <simon@simon.geek.nz>
2013-04-08 21:32:05 +1200

    Update pl_PL.js

--- javascript/lang/pl_PL.js
+++ javascript/lang/pl_PL.js
@@ -17,7 +17,7 @@ if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
 		'CMSMAIN.PUBLISHING' : 'Publikacja...',
 		'CMSMAIN.RESTORING': 'Odzyskiwanie...',
 		'CMSMAIN.ERRORREVERTING': 'Błąd podczas powrotu do opublikowanej strony',
-		'CMSMAIN.SAVING' : 'Zapisywanie...'
+		'CMSMAIN.SAVING' : 'Zapisywanie...',
 		'CMSMAIN.SELECTMOREPAGES' : "Zaznaczono %s stron.\n\nCzy na pewno chcesz wykonać tę akcje?",
 		'CMSMAIN.ALERTCLASSNAME': 'Ta strona zostanie zaktualizowana po jej zapisani'ur,
 		'CMSMAIN.URLSEGMENTVALIDATION': 'Adres URL może składać się tylko z liter, cyfr i łączników.',
