d4304f0d8efeec186776a51f1fc40f8f3d9dd675
Sean Harvey <sean@silverstripe.com>
2013-04-12 13:26:05 +1200

    Don't set locale in mysite/_config.php, it defaults in cwp/_config.php

--- mysite/_config.php
+++ mysite/_config.php
@@ -18,7 +18,6 @@ MySQLDatabase::set_connection_charset('utf8');
 
 SSViewer::set_theme('default');
 
-i18n::set_locale('en_NZ');
 date_default_timezone_set('Pacific/Auckland');
 
 if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
