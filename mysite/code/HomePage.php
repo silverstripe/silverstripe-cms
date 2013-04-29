5595d131821cdc6603e2b715c3b0c877108f1d04
Robert Curry <robert@silverstripe.com>
2013-04-29 15:51:56 +1200

    Add description to homepage

--- mysite/code/HomePage.php
+++ mysite/code/HomePage.php
@@ -1,7 +1,7 @@
 <?php
 
 class HomePage extends BaseHomePage {
-	
+	private static $description = 'Entry to the site. Has carousel, featured panels, quicklinks and news summaries';
 }
 
 class HomePage_Controller extends BaseHomePage_Controller {
