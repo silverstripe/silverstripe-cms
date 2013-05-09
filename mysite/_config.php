07a1fdfcfdc38ff062e875d125c60ddffa1fbd49
Sean Harvey <sean@silverstripe.com>
2013-05-09 15:14:01 +1200

    Removing proxy code from mysite, it's in cwp module now

--- mysite/_config.php
+++ mysite/_config.php
@@ -14,16 +14,6 @@ if (defined('SS_DATABASE_NAME') && SS_DATABASE_NAME) {
 
 require_once('conf/ConfigureFromEnv.php');
 
-// redirect some requests to the secure domain
-if(defined('CWP_SECURE_DOMAIN') && @$_SERVER['HTTP_X_FORWARDED_PROTOCOL'] != 'https') {
-	Director::forceSSL(array('/^Security/'), CWP_SECURE_DOMAIN);
-	// Note 1: the platform always redirects "/admin" to CWP_SECURE_DOMAIN regardless of what you set here
-	// Note 2: if you have your own certificate installed, you can use your own domain, just omit the second parameter:
-	//   Director::forceSSL(array('/^Security/'));
-	//
-	// See Director::forceSSL for more information.
-}
-
 MySQLDatabase::set_connection_charset('utf8');
 
 SSViewer::set_theme('default');
