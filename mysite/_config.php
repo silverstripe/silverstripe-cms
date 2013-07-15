872fa6127765804e637624ec92150ded96ae8e15
Mateusz Uzdowski <mateusz@silverstripe.com>
2013-07-16 09:08:09 +1200

    Add default admin email to populate the FROM field.
    Emails get caught by spam filters.

--- mysite/_config.php
+++ mysite/_config.php
@@ -16,6 +16,9 @@ require_once('conf/ConfigureFromEnv.php');
 
 date_default_timezone_set('Pacific/Auckland');
 
+// Set default FROM field for emails. Reconfigure this if you would like to use a custom address.
+Email::setAdminEmail('admin@cwp.govt.nz');
+
 ## NOTE: Any SilverStripe configuration ideally goes into mysite/_config/config.yml
 ## which uses the {@link Config} API instead of manipulating statics directly.
 ## Check out "configuration.md" in the framework docs for more information.
