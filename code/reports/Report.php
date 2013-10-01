silverstripe/silverstripe-cms

commit 79996a76fe55bd95a99dd99aae5c5f27626f0f06

Clearer escaping in ReportAdmin
No direct security issue since report titles can't be set by the user

code/reports/Report.php
@@ -301,8 +301,13 @@ class SS_Report extends ViewableData {
 			if(isset($info['casting'])) $fieldCasting[$source] = $info['casting'];
 
 			if(isset($info['link']) && $info['link']) {
-				$link = singleton('CMSPageEditController')->Link('show');
-				$fieldFormatting[$source] = '<a href=\"' . $link . '/$ID\">$value</a>';
+				$fieldFormatting[$source] = function($value, &$item) {
+					return sprintf(
+						'<a href=\"%s\">%s</a>',
+						Controller::join_links(singleton('CMSPageEditController')->Link('show'), $item->ID),
+						Convert::raw2xml($value)
+					);
+				};
 			}
 
 			$displayFields[$source] = isset($info['title']) ? $info['title'] : $source;

code/controllers/ReportAdmin.php
@@ -166,7 +166,13 @@ class ReportAdmin extends LeftAndMain implements PermissionProvider {
 				'title' => _t('ReportAdmin.ReportTitle', 'Title'),
 			));
 			$columns->setFieldFormatting(array(
-				'title' => '<a href=\"$Link\" class=\"cms-panel-link\">$value</a>'
+				'title' => function($value, &$item) {
+					return sprintf(
+						'<a href=\"%s\" class=\"cms-panel-link\">%s</a>',
+						Convert::raw2xml($item->Link),
+						Convert::raw2xml($value)
+					);
+				}
 			));
 			$gridField->addExtraClass('all-reports-gridfield');
 			$fields->push($gridField);

commit d941a0281ff06bd2cbc896f35f290ebc34a90f75

FIX Report links regression
Regression from 79996a7

code/reports/Report.php
@@ -303,7 +303,7 @@ class SS_Report extends ViewableData {
 			if(isset($info['link']) && $info['link']) {
 				$fieldFormatting[$source] = function($value, &$item) {
 					return sprintf(
-						'<a href=\"%s\">%s</a>',
+						'<a href="%s">%s</a>',
 						Controller::join_links(singleton('CMSPageEditController')->Link('show'), $item->ID),
 						Convert::raw2xml($value)
 					);