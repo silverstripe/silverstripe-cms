silverstripe/silverstripe-cms

commit 79996a76fe55bd95a99dd99aae5c5f27626f0f06

Clearer escaping in ReportAdmin
No direct security issue since report titles can't be set by the user

code/controllers/ReportAdmin.php
@@ -166,7 +166,13 @@ class ReportAdmin extends LeftAndMain implements PermissionProvider {
 				'title' => _t('ReportAdmin.ReportTitle', 'Title'),
 			));
 			$columns->setFieldFormatting(array(
-				'title' => '<a href=\"$Link\" class=\"cms-panel-link\">$value</a>'
+				'title' => function($value, &$item) {
+					return sprintf(
+						'<a href=\"%s\" class=\"cms-panel-link\">%s</a>',
+						Convert::raw2xml($item->Link),
+						Convert::raw2xml($value)
+					);
+				}
 			));
 			$gridField->addExtraClass('all-reports-gridfield');
 			$fields->push($gridField);

code/reports/Report.php
@@ -301,8 +301,13 @@ class SS_Report extends ViewableData {
 			if(isset($info['casting'])) $fieldCasting[$source] = $info['casting'];
 
 			if(isset($info['link']) && $info['link']) {
-				$link = singleton('CMSPageEditController')->Link('show');
-				$fieldFormatting[$source] = '<a href=\"' . $link . '/$ID\">$value</a>';
+				$fieldFormatting[$source] = function($value, &$item) {
+					return sprintf(
+						'<a href=\"%s\">%s</a>',
+						Controller::join_links(singleton('CMSPageEditController')->Link('show'), $item->ID),
+						Convert::raw2xml($value)
+					);
+				};
 			}
 
 			$displayFields[$source] = isset($info['title']) ? $info['title'] : $source;

commit daf92e6ce0313b6a2debef6e1b03550104202e99

FIX ReportAdmin report links regression
Regression from 79996a76fe55bd95a99dd99aae5c5f27626f0f06,
escaped quotes are no longer necessary because the string
doesn't get eval'ed.

code/controllers/ReportAdmin.php
@@ -168,7 +168,7 @@ class ReportAdmin extends LeftAndMain implements PermissionProvider {
 			$columns->setFieldFormatting(array(
 				'title' => function($value, &$item) {
 					return sprintf(
-						'<a href=\"%s\" class=\"cms-panel-link\">%s</a>',
+						'<a href="%s" class="cms-panel-link">%s</a>',
 						Convert::raw2xml($item->Link),
 						Convert::raw2xml($value)
 					);

tests/behat/features/view-reports.feature
@@ -1,0 +1,15 @@
+Feature: View Reports
+  As an author
+  I want to review aggregate reports on pages
+  So that I can keep an overview on the health of my website data
+
+  Background:
+    Given a "page" "Empty Page"
+    And a "page" "Filled Page" with "Content"="Some Content"
+    And I am logged in with "ADMIN" permissions
+    And I go to "/admin/reports"
+
+  Scenario: I can view the "Pages with no content" report
+    When I follow "Pages with no content"
+    Then I should see "Empty Page"
+    But I should not see "Filled Page"