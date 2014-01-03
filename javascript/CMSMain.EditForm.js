silverstripe/silverstripe-cms

commit 1571d3542d237ed2958711f0254013d5859f6642

BUG Only use $.button in onunmatch if init'ed (fixes #8181)

javascript/CMSMain.EditForm.js
@@ -321,8 +321,10 @@
 				this._super(e);
 			},
 			onunmatch: function(e) {
-				this.find('button[name=action_save]').button('option', 'showingAlternate', false);
-				this.find('button[name=action_publish]').button('option', 'showingAlternate', false);
+				var saveButton = this.find('button[name=action_save]');
+				if(saveButton.data('button')) saveButton('option', 'showingAlternate', false);
+				var publishButton = this.find('button[name=action_publish]');
+				if(publishButton.data('button')) publishButton('option', 'showingAlternate', false);
 				this._super(e);
 			}
 		});

commit 034bfc35c4cbe551c52831de54b53c2f4da8e7e8

FIX: Changetracker causing 'saveButton is not a function' error
Regression from 1571d35

javascript/CMSMain.EditForm.js
@@ -288,9 +288,9 @@
 			},
 			onunmatch: function(e) {
 				var saveButton = this.find('button[name=action_save]');
-				if(saveButton.data('button')) saveButton('option', 'showingAlternate', false);
+				if(saveButton.data('button')) saveButton.button('option', 'showingAlternate', false);
 				var publishButton = this.find('button[name=action_publish]');
-				if(publishButton.data('button')) publishButton('option', 'showingAlternate', false);
+				if(publishButton.data('button')) publishButton.button('option', 'showingAlternate', false);
 				this._super(e);
 			}
 		});