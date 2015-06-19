04a513b0a8eba28f80fbe86a7dc28fd2d4ed187f
Damian Mooyman <damian@silverstripe.com>
2015-06-19 12:00:42 +1200

    Set deprecation level for all changes in 3.x to 4.0

--- code/model/ErrorPage.php
+++ code/model/ErrorPage.php
@@ -300,20 +300,20 @@ class ErrorPage extends Page {
 	 * Set the path where static error files are saved through {@link publish()}.
 	 * Defaults to /assets.
 	 *
-	 * @deprecated 3.2 Use "ErrorPage.static_file_path" instead
+	 * @deprecated 4.0 Use "ErrorPage.static_file_path" instead
 	 * @param string $path
 	 */
 	static public function set_static_filepath($path) {
-		Deprecation::notice('3.2', 'Use "ErrorPage.static_file_path" instead');
+		Deprecation::notice('4.0', 'Use "ErrorPage.static_file_path" instead');
 		self::config()->static_filepath = $path;
 	}
 	
 	/**
-	 * @deprecated 3.2 Use "ErrorPage.static_file_path" instead
+	 * @deprecated 4.0 Use "ErrorPage.static_file_path" instead
 	 * @return string
 	 */
 	static public function get_static_filepath() {
-		Deprecation::notice('3.2', 'Use "ErrorPage.static_file_path" instead');
+		Deprecation::notice('4.0', 'Use "ErrorPage.static_file_path" instead');
 		return self::config()->static_filepath;
 	}
 }
