bf9939e32c1e624fdff4de8b0ca213531093a496
Sam Minnee <sam@silverstripe.com>
2016-10-28 17:47:30 +1300

    NEW: Refactor test bootstrap to be more modular.
    The test bootstrap is broken into units that can be combined for
    different test situations:
    
     - phpunit
     - behat
     - serve

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . '/bootstrap/init.php';
+require FRAMEWORK_PATH . '/tests/bootstrap/init.php';
+require FRAMEWORK_PATH . '/tests/bootstrap/cli.php';
+require FRAMEWORK_PATH . '/tests/bootstrap/environment.php';
+require __DIR__ . '/bootstrap/mysite.php';
+require FRAMEWORK_PATH . '/tests/bootstrap/mysite.php';
+require FRAMEWORK_PATH . '/tests/bootstrap/phpunit.php';
+
