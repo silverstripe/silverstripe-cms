0980c62c7c5699e13a51e6c72a4c3f35ca792510
Damian Mooyman <damian@silverstripe.com>
2017-05-03 17:17:51 +1200

    Rebuild dist files

--- client/lang/sr.js
+++ client/lang/sr.js
@@ -7,19 +7,19 @@ if (typeof(ss) === 'undefined' || typeof(ss.i18n) === 'undefined') {
 } else {
   ss.i18n.addDictionary('sr', {
     "CMS.ALERTCLASSNAME": "Тип странице ће бити ажуриран након што сачувате страницу",
+    "CMS.AddSubPage": "Додај нову страницу овде",
     "CMS.ConfirmRestoreFromLive": "Are you sure you want to revert draft to when the page was last published?",
+    "CMS.Duplicate": "Дуплирај/копирај",
+    "CMS.EditPage": "Измени",
+    "CMS.ONLYSELECTTWO": "You can only compare two versions at this time.",
     "CMS.Restore": "Are you sure you want to restore this page from archive?",
     "CMS.RestoreToRoot": "Are you sure you want to restore this page from archive?\n\nBecause the parent page is not available this will be restored to the top level.",
     "CMS.RollbackToVersion": "Да ли заиста желите да се вратите на верзију  #%s ове странице?",
-    "CMS.Unpublish": "Are you sure you want to remove your page from the published site?\n\nThis page will still be available in the sitetree as draft.",
-    "CMS.AddSubPage": "Додај нову страницу овде",
-    "CMS.Duplicate": "Дуплирај/копирај",
-    "CMS.EditPage": "Измени",
     "CMS.ShowAsList": "Прикажу децу у виду листе",
     "CMS.ThisPageAndSubpages": "Ова страница и подстранице",
     "CMS.ThisPageOnly": "Само ова страница",
-    "CMS.ViewPage": "View",
+    "CMS.Unpublish": "Are you sure you want to remove your page from the published site?\n\nThis page will still be available in the sitetree as draft.",
     "CMS.UpdateURL": "Update URL",
-    "CMS.ONLYSELECTTWO": "You can only compare two versions at this time."
+    "CMS.ViewPage": "View"
 });
 }
