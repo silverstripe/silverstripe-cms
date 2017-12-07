7eb04121a6cfbe7832dd9efd292155ae3b62c2a1
Lee Bradley <oilee80@users.noreply.github.com>
2017-12-07 02:36:51 +0000

    Change `new HTMLEditorField` to `HTMLEditorField::Create` (#2050)
    This allows for the field to be overridden using
      the injector e.g.
    
    ```yml
    
    SilverStripe\Core\Injector\Injector:
      SilverStripe\Forms\HTMLEditor\HTMLEditorField:
        class: SilverStripers\markdown\forms\MarkdownEditorField
    ```

--- code/Model/SiteTree.php
+++ code/Model/SiteTree.php
@@ -1855,7 +1855,7 @@ class SiteTree extends DataObject implements PermissionProvider, i18nEntityProvi
                     new TextField("Title", $this->fieldLabel('Title')),
                     $urlsegment,
                     new TextField("MenuTitle", $this->fieldLabel('MenuTitle')),
-                    $htmlField = new HTMLEditorField("Content", _t(__CLASS__.'.HTMLEDITORTITLE', "Content", 'HTML editor title')),
+                    $htmlField = HTMLEditorField::create("Content", _t(__CLASS__.'.HTMLEDITORTITLE', "Content", 'HTML editor title')),
                     ToggleCompositeField::create(
                         'Metadata',
                         _t(__CLASS__.'.MetadataToggle', 'Metadata'),
