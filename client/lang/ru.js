e84e0236f18962d47de961fe0401bbae0c1716b9
Damian Mooyman <damian@silverstripe.com>
2018-02-05 16:58:21 +1300

    Update translations

--- client/lang/ru.js
+++ client/lang/ru.js
@@ -8,10 +8,10 @@ if (typeof(ss) === 'undefined' || typeof(ss.i18n) === 'undefined') {
   ss.i18n.addDictionary('ru', {
     "CMS.ALERTCLASSNAME": "Тип страницы будет изменён после её сохранения.",
     "CMS.AddSubPage": "Добавить новую страницу",
-    "CMS.ConfirmRestoreFromLive": "Вы действительно хотите вернуть страницу к состоянию опубликованной версии?",
+    "CMS.ConfirmRestoreFromLive": "Вы действительно хотите вернуть черновик к состоянию опубликованной версии?",
     "CMS.Duplicate": "Создать копию",
     "CMS.EditPage": "Редактировать",
-    "CMS.ONLYSELECTTWO": "You can only compare two versions at this time.",
+    "CMS.ONLYSELECTTWO": "Вы можете сравнивать только две версии.",
     "CMS.Restore": "Вы действительно хотите восстановить эту страницу из архива?",
     "CMS.RestoreToRoot": "Вы уверены что хотите восстановить данную страницу из архива?\n\nПоскольку родительская страница недоступна данная страница будет восстановлена в корень сайта.",
     "CMS.RollbackToVersion": "Вы действительно хотите восстановить версию #%s этой страницы?",
@@ -19,7 +19,7 @@ if (typeof(ss) === 'undefined' || typeof(ss.i18n) === 'undefined') {
     "CMS.ThisPageAndSubpages": "Вместе с вложенными страницами",
     "CMS.ThisPageOnly": "Только этой страницы",
     "CMS.Unpublish": "Вы уверены что хотите удалить страницу с опубликованного сайта?\n\nСтраница останется в черновиках.",
-    "CMS.UpdateURL": "Обновить Адрес",
+    "CMS.UpdateURL": "Обновить URL",
     "CMS.ViewPage": "Просмотр"
 });
 }
