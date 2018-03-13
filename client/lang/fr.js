9bf05b397f1bcb0522fa6e773f12be394f3ce7e5
Damian Mooyman <damian@silverstripe.com>
2018-03-14 09:45:54 +1300

    Update translations

--- client/lang/fr.js
+++ client/lang/fr.js
@@ -11,7 +11,7 @@ if (typeof(ss) === 'undefined' || typeof(ss.i18n) === 'undefined') {
     "CMS.ConfirmRestoreFromLive": "Are you sure you want to revert draft to when the page was last published?",
     "CMS.Duplicate": "Dupliquer",
     "CMS.EditPage": "Editer",
-    "CMS.ONLYSELECTTWO": "You can only compare two versions at this time.",
+    "CMS.ONLYSELECTTWO": "Vous ne pouvez comparez que deux versions pour le moment.",
     "CMS.Restore": "Are you sure you want to restore this page from archive?",
     "CMS.RestoreToRoot": "Are you sure you want to restore this page from archive?\n\nBecause the parent page is not available this will be restored to the top level.",
     "CMS.RollbackToVersion": "Voulez-vous vraiment retourner à la version #%s de cette page ?",
@@ -19,7 +19,7 @@ if (typeof(ss) === 'undefined' || typeof(ss.i18n) === 'undefined') {
     "CMS.ThisPageAndSubpages": "Cette page et ses sous-pages",
     "CMS.ThisPageOnly": "Seulement cette page",
     "CMS.Unpublish": "Are you sure you want to remove your page from the published site?\n\nThis page will still be available in the sitetree as draft.",
-    "CMS.UpdateURL": "Update URL",
+    "CMS.UpdateURL": "Mettre à jour URL",
     "CMS.ViewPage": "View"
 });
 }
