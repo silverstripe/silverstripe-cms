3cdccc331df1a9a3cfe2e675b98ed343984d171a
Damian Mooyman <damian@silverstripe.com>
2018-03-28 16:47:59 +1300

    Ensure upgrade task occurs on draft

--- code/Tasks/MigrateSiteTreeLinkingTask.php
+++ code/Tasks/MigrateSiteTreeLinkingTask.php
@@ -6,6 +6,7 @@ use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Dev\BuildTask;
 use SilverStripe\Dev\Debug;
 use SilverStripe\ORM\DB;
+use SilverStripe\Versioned\Versioned;
 
 /**
  * Updates legacy SiteTree link tracking into new polymorphic many_many relation.
@@ -25,20 +26,27 @@ class MigrateSiteTreeLinkingTask extends BuildTask
         $exists = DB::get_conn()->getSchemaManager()->hasTable('SiteTree_LinkTracking');
         if (!$exists) {
             DB::alteration_message("Table SiteTree_LinkTracking has already been migrated, or doesn't exist");
+            return;
         }
 
         $pages = 0;
-        /** @var SiteTree[] $linkedPages */
-        $linkedPages = SiteTree::get()
-            ->innerJoin(
-                'SiteTree_LinkTracking',
-                '"SiteTree_LinkTracking"."SiteTreeID" = "SiteTree"."ID"'
-            );
-        foreach ($linkedPages as $page) {
-            // Command page to update symlink tracking
-            $page->syncLinkTracking();
-            $pages++;
-        }
+
+        // Ensure sync occurs on draft
+        Versioned::withVersionedMode(function() use (&$pages) {
+            Versioned::set_stage(Versioned::DRAFT);
+
+            /** @var SiteTree[] $linkedPages */
+            $linkedPages = SiteTree::get()
+                ->innerJoin(
+                    'SiteTree_LinkTracking',
+                    '"SiteTree_LinkTracking"."SiteTreeID" = "SiteTree"."ID"'
+                );
+            foreach ($linkedPages as $page) {
+                // Command page to update symlink tracking
+                $page->syncLinkTracking();
+                $pages++;
+            }
+        });
         DB::alteration_message("Migrated page links on " . SiteTree::singleton()->i18n_pluralise($pages));
 
         // Disable table to prevent double-migration
