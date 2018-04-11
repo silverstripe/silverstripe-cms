5da708d223723f91ffe47672846a3967c52a5326
Robbie Averill <robbie@silverstripe.com>
2018-04-11 19:03:09 +1200

    FIX Fixture the 'app' dir instead of mysite in unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,4 +4,4 @@ require __DIR__ . '/bootstrap/init.php';
 require FRAMEWORK_PATH . '/tests/bootstrap/init.php';
 require FRAMEWORK_PATH . '/tests/bootstrap/cli.php';
 require FRAMEWORK_PATH . '/tests/bootstrap/environment.php';
-require __DIR__ . '/bootstrap/mysite.php';
+require __DIR__ . '/bootstrap/app.php';
