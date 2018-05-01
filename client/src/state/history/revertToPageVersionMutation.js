2b5ef3fac40412fe8ecc9b80a064df0ed651da98
Robbie Averill <robbie@silverstripe.com>
2018-05-01 18:56:08 +1200

    NEW Add revert mutation and refactor injector transformations
    Removing the existing HOC component and just using Injector transformations to apply the GraphQL query and mutation

---
+++ client/src/state/history/revertToPageVersionMutation.js
@@ -0,0 +1,40 @@
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const mutation = gql`
+mutation revertPageToVersion($id:ID!, $fromStage:VersionedStage!, $toStage:VersionedStage!, $fromVersion:Int!) {
+  copySilverStripeSiteTreeToStage(Input: {
+    ID: $id
+    FromVersion: $fromVersion
+    FromStage: $fromStage
+    ToStage: $toStage
+  }) {
+    ID
+  }
+}
+
+`;
+
+const config = {
+  props: ({ mutate, ownProps: { actions } }) => {
+    const revertToVersion = (id, fromVersion, fromStage, toStage) => mutate({
+      variables: {
+        id,
+        fromVersion,
+        fromStage,
+        toStage,
+      },
+    });
+
+    return {
+      actions: {
+        ...actions,
+        revertToVersion,
+      },
+    };
+  },
+};
+
+export { mutation, config };
+
+export default graphql(mutation, config);
