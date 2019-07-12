cb737aa842a3b980c709e564d98846c1cf4310e6
Aaron Carlino <unclecheese@leftandmain.com>
2019-07-12 15:31:17 +1200

    API: Remove CMSPageHistoryViewer

--- client/src/bundles/bundle.js
+++ client/src/bundles/bundle.js
@@ -2,7 +2,6 @@ require('../legacy/CMSMain.AddForm.js');
 require('../legacy/CMSMain.EditForm.js');
 require('../legacy/CMSMain.js');
 require('../legacy/CMSMain.Tree.js');
-require('../legacy/CMSPageHistoryController.js');
 require('../legacy/RedirectorPage.js');
 require('../legacy/SiteTreeURLSegmentField.js');
 
