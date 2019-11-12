7045082a06e56d5a4b5070475998a929a41b0452
Adrian Humphreys <adrian.humphreys@silverstripe.com>
2019-11-13 10:21:52 +1300

    BUG Cache page icons (#2493)

--- code/Controllers/LeftAndMainPageIconsExtension.php
+++ code/Controllers/LeftAndMainPageIconsExtension.php
@@ -2,32 +2,59 @@
 
 namespace SilverStripe\CMS\Controllers;
 
+use Psr\SimpleCache\CacheInterface;
+use Psr\SimpleCache\InvalidArgumentException;
+use ReflectionException;
 use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Core\ClassInfo;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\Core\Convert;
 use SilverStripe\Core\Extension;
+use SilverStripe\Core\Flushable;
+use SilverStripe\Core\Injector\Injector;
 use SilverStripe\View\Requirements;
 
 /**
  * Extension to include custom page icons
  */
-class LeftAndMainPageIconsExtension extends Extension
+class LeftAndMainPageIconsExtension extends Extension implements Flushable
 {
 
+    /**
+     * @throws InvalidArgumentException
+     * @throws ReflectionException
+     */
     public function init()
     {
         Requirements::customCSS($this->generatePageIconsCss(), CMSMain::PAGE_ICONS_ID);
     }
 
     /**
+     * Just broadly clears the cache on flush
+     */
+    public static function flush()
+    {
+        Injector::inst()->get(CacheInterface::class . '.SiteTree_PageIcons')->clear();
+    }
+
+
+    /**
      * Include CSS for page icons. We're not using the JSTree 'types' option
      * because it causes too much performance overhead just to add some icons.
      *
      * @return string CSS
+     * @throws InvalidArgumentException
+     * @throws ReflectionException
      */
     public function generatePageIconsCss()
     {
+        /** @var CacheInterface $cache */
+        $cache = Injector::inst()->get(CacheInterface::class . '.SiteTree_PageIcons');
+
+        if ($cache->has('css')) {
+            return $cache->get('css');
+        }
+
         $css = '';
         $classes = ClassInfo::subclassesFor(SiteTree::class);
         foreach ($classes as $class) {
@@ -41,6 +68,9 @@ class LeftAndMainPageIconsExtension extends Extension
                 $css .= sprintf('%s { background: transparent url(\'%s\') 0 0 no-repeat; }', $selector, $iconURL);
             }
         }
+
+        $cache->set('css', $css);
+
         return $css;
     }
 }
