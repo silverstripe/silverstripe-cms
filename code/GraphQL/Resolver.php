369e0896af557887bd807af71124fb7d41cfed9c
Aaron Carlino <unclecheese@leftandmain.com>
2020-09-02 14:19:19 +1200

    Add getPageByLink

---
+++ code/GraphQL/Resolver.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace SilverStripe\CMS\GraphQL;
+
+
+use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\GraphQL\Schema\Resolver\DefaultResolverProvider;
+
+if (!class_exists(DefaultResolverProvider::class)) {
+    return;
+}
+
+class Resolver extends DefaultResolverProvider
+{
+    public static function resolveGetPageByLink($obj, array $args = [])
+    {
+        return SiteTree::get_by_link($args['link']);
+    }
+}
