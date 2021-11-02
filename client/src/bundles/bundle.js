90a8fcfa7e7a37cfa126bf0e380712428e546c65
Steve Boyd <emteknetnz@gmail.com>
2021-11-02 21:55:54 +1300

    MNT Remove js prefix from require statements

--- client/src/bundles/bundle.js
+++ client/src/bundles/bundle.js
@@ -1,9 +1,9 @@
-require('../legacy/CMSMain.AddForm.js');
-require('../legacy/CMSMain.EditForm.js');
-require('../legacy/CMSMain.js');
-require('../legacy/CMSMain.Tree.js');
-require('../legacy/CMSPageHistoryController.js');
-require('../legacy/RedirectorPage.js');
-require('../legacy/SiteTreeURLSegmentField.js');
+require('../legacy/CMSMain.AddForm');
+require('../legacy/CMSMain.EditForm');
+require('../legacy/CMSMain');
+require('../legacy/CMSMain.Tree');
+require('../legacy/CMSPageHistoryController');
+require('../legacy/RedirectorPage');
+require('../legacy/SiteTreeURLSegmentField');
 
 require('boot');
