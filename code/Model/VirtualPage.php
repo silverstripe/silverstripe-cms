9edc46f53dfd2cf62ee0e8d6634c6a33366fbfa3
mirche97 <mirche97@gmail.com>
2023-09-18 18:15:54 +0200

    Update code/Model/VirtualPage.php
    Co-authored-by: Guy Sartorelli <36352093+GuySartorelli@users.noreply.github.com>

--- code/Model/VirtualPage.php
+++ code/Model/VirtualPage.php
@@ -248,7 +248,7 @@ class VirtualPage extends Page
                     'a',
                     [
                         'class' => 'cmsEditlink',
-                        'href' => AdminRootController::get_admin_route() . '/pages/edit/show/' . $this->CopyContentFromID,
+                        'href' => $this->CopyContentFrom()->CMSEditLink(),
                     ],
                     _t(self::class . '.EditLink', 'edit')
                 );
